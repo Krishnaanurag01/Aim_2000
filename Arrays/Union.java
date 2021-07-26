3d0dc996f9f050252d108f0d5003fad7257896d0
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-07-26 22:24:20 +0530

    DSA Problems

---
+++ Arrays/Union.java
@@ -0,0 +1,18 @@
+package Arrays;
+public class Union {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {2,4,6,8};        
+        System.out.println(getUnion(arr1, arr2));
+    }
+    public static int getUnion(int[] arr1, int[] arr2) {
+        int unionCount = 0;
+        int ptr1=0 , ptr2=0;
+        while(ptr1 < arr1.length && ptr2 < arr2.length){
+            if
+        }
+        return unionCount;
+        
+    }
+    
+}
