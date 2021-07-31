32319c4abbcd46fd8bb7a7d0b3f7aac4a62ad997
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-07-31 22:26:53 +0530

    DSA Problems

---
+++ Arrays/RearraningArrayInAlternatePosition_Q25.java
@@ -0,0 +1,16 @@
+package Arrays;
+public class RearraningArrayInAlternatePosition_Q25 {
+    public static void main(String[] args) {
+        int[] arr= {1, 2, 3, -4, -1, 4};
+    }
+    public static void rearrangeIt(int[] arr) {
+        int[] temp = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < temp.length; j++) {
+                if()
+            }
+        }
+        
+    }
+}
