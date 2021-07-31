9b7a89df80c1a2b2f5feaf6fc7418fea07cab3c5
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-07-31 22:27:08 +0530

    DSA Problems

--- Arrays/CountPairsWithGivenSum_Q23.java
+++ Arrays/CountPairsWithGivenSum_Q23.java
@@ -1,31 +1,69 @@
 package Arrays;
 
-import java.util.Arrays;
+
+import java.util.HashMap;
+
 
 public class CountPairsWithGivenSum_Q23 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5,7,4};
-        System.out.println(getCountOfPairs(arr,7));
+        int[] arr = {10, 12, 10, 15, -1, 7, 6, 
+            5, 4, 2, 1, 1, 1};
+        
+        System.out.println(getCountOfPairs(arr,11));
+        
     }
-    public static int getCountOfPairs(int[] arr, int k) {
-        int i = 0;
-        int j = arr.length-1;
-        int count = 0;
-        Arrays.sort(arr); 
-        while (i<j) {
-            if(arr[i] + arr[j] > k){
-                j--;
-            }
-            else if(arr[i] + arr[j] < k){
-                i++;
-            }
-            if(arr[i]+ arr[j] == k)
-            {
-                count++;
-                continue;
-            }
 
+// brute force. o(n2)
+// use neested loop for that.
+
+
+    // // time complexicity : o(nlogn + n)
+
+    // getting error in this.
+
+    // public static int getCountOfPairsUsingWhile(int[] arr, int k) {
+    //     int i = 0;
+    //     int j = arr.length-1;
+    //     int count = 0;
+    //     Arrays.sort(arr); 
+    //     while (i < j) {
+    //         if(arr[i] + arr[j] > k){
+    //             j--;
+    //         }
+    //         else if(arr[i] + arr[j] < k){
+    //             i++;
+    //         }
+    //         if(arr[i]+ arr[j] == k)
+    //         {
+    //             count++;
+    //             i++;
+            
+    //         }
+
+    //         }
+    //     return count;
+    // }
+
+
+
+    // time complexicty : o(n)
+
+    public static int getCountOfPairs(int[] arr , int k) {
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int element = k - arr[i];
+            if(map.containsKey(element)){
+                count += map.get(element);
             }
+            if( map.get(arr[i]) == null){
+                map.put(arr[i] , 0);
+            }
+            map.put(arr[i], map.get(arr[i])+1);
+        }
+
+        
         return count;
+        
     }
 }
