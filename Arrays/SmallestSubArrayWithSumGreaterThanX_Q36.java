b24e16e006bc3543fc7657c550719f35c9cadb56
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-05 22:42:57 +0530

    DSA Problems

---
+++ Arrays/SmallestSubArrayWithSumGreaterThanX_Q36.java
@@ -0,0 +1,48 @@
+package Arrays;
+/**
+ * SmallestSubArrayWithSumGreaterThanX_Q36
+ */
+public class SmallestSubArrayWithSumGreaterThanX_Q36 {
+
+    public static void main(String[] args) {
+        
+        int[] arr= {1, 4, 45, 6, 0, 19};
+        int X= 51;
+        System.out.println(getLengthOfSmallestSubArrayGreaterThanX(arr, arr.length , X));
+    }
+    
+    public static int getLengthOfSmallestSubArrayGreaterThanX(int[] arr , int n , int x) {{
+
+        
+        int start = 0 ; 
+        int end  =1 ;
+        int answer = Integer.MAX_VALUE ;
+        int sum  = arr[start];
+        if(sum > x ) return 1;
+        if(end < n) sum += arr[end];
+
+        while(start < n && end < n){
+            if(sum > x){
+                answer = Math.min(answer, end - start +1);
+                sum -= arr[start];
+                start++;
+
+            }
+            else{
+                end++;
+                if(end < n){
+                    sum += arr[end];
+                }
+            }
+        }
+
+
+    
+        
+        
+        
+        return answer;
+    }
+        
+    }
+}
