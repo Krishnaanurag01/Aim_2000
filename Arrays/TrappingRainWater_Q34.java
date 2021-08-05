5fd3e270025775a1d47d5f533bd29a2a6e6e7791
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-05 12:12:01 +0530

    DSA Problems

---
+++ Arrays/TrappingRainWater_Q34.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+
+public class TrappingRainWater_Q34 {
+    public static void main(String[] args) {
+        int[] arr = {7, 4 ,0 ,9} ;
+        System.out.println(getTrappedWaterSize(arr, arr.length));
+    }
+    public static int getTrappedWaterSize(int[] arr, int n) {
+
+       int[] leftMax = new int[n];
+       int[] rightMax = new int[n];
+
+       leftMax[0] = arr[0];
+       
+       for (int i = 1; i < n; i++) {
+           leftMax[i] = Math.max(arr[i], leftMax[i-1]);
+        }
+
+        rightMax[n-1] = arr[n-1];
+
+       for (int i = n-2 ; i >= 0 ; i--) {
+        rightMax[i] = Math.max(arr[i], rightMax[i+1]);
+       }
+
+       int trappedWater  = 0;
+
+       for (int i = 0; i < n; i++) {
+           trappedWater += Math.min(leftMax[i],rightMax[i]) - arr[i];
+           
+       }
+
+       for (int i : leftMax) {
+           System.out.print(i+" ");
+        }
+        System.out.println();
+       for (int i : rightMax) {
+           System.out.print(i+" ");
+       }
+       return trappedWater;
+
+    }
+}
