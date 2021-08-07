618f6777c0f422b1b76d15fb1a28d6a85bcf6ff8
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-07 22:33:23 +0530

    DSA Problems

--- Arrays/MinimumNoOfJumpToReachEndOfArray.java
+++ Arrays/MinimumNoOfJumpToReachEndOfArray.java
@@ -4,8 +4,10 @@ public class MinimumNoOfJumpToReachEndOfArray {
     public static void main(String[] args) {
         int[] arr ={1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
         System.out.println(getMinJump(arr,arr.length));
+        System.out.println(getTotalJumps(arr,arr.length));
     }
 
+// this algorithm taking extra time in traversing every element.
     public static int getMinJump(int[] arr,int n) {
         int maxReach = arr[0];
         int steps = arr[0];
@@ -32,4 +34,37 @@ public class MinimumNoOfJumpToReachEndOfArray {
 
         
     }
+
+
+// this algorithm is much more effecient and optimized as it does not traversed every element unless and until it get the max value.
+
+public static int getTotalJumps(int[] arr , int n) {
+
+    int max = 0 , halt = 0 , jump =0 ;
+
+    if(arr[0] == 0){
+        return -1;
+    }
+
+    for (int i = 0; i < n-1; i++) {
+        max = Math.max(max, arr[i]+1);
+        
+        if(max >= n-1){
+            jump++;
+            return jump;
+        }
+        if(halt == i){
+            halt = max;
+            jump++;
+        }
+    }
+    
+    if(halt>=  n-1){
+        return jump;
+    }
+
+
+    return -1;
+    
+}
 }
