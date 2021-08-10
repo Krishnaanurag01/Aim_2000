ed8d3cc7ad1eb1fcc8bebdae98b1d8c1691fd6b6
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-10 22:20:26 +0530

    DSA Problems

--- Matrix/KthSmallestElementInMatrix_Q10.java
+++ Matrix/KthSmallestElementInMatrix_Q10.java
@@ -8,20 +8,22 @@ public class KthSmallestElementInMatrix_Q10 {
         
     }
 
+    // will upload soon.
+
     public static int getKthSmallestElement(int[][] matrix , int k ) {
         
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        // PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
 
-        for (int i = 0; i < k ; i++) {
-            queue.add(matrix[i][i])
-        }
+        // for (int i = 0; i < k ; i++) {
+        //     queue.add(matrix[i][i])
+        // }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                queue.add(matrix[i][j]);
-            }
-        }
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[i].length; j++) {
+        //         queue.add(matrix[i][j]);
+        //     }
+        // }
 
 
 
