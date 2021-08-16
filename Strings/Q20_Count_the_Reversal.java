e59116566bc23fb5f434fbddafb1d0e958c4bd32
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-16 21:44:29 +0530

    DSA Problems

---
+++ Strings/Q20_Count_the_Reversal.java
@@ -0,0 +1,43 @@
+package Strings;
+
+import java.util.Stack;
+
+public class Q20_Count_the_Reversal {
+    public static void main(String[] args) {
+
+      System.out.println( countTheReversal("{}{{}{{{{{}}{}") );
+    
+    }
+
+    public static int countTheReversal(String s) { 
+
+        if(s.length()%2 != 0){
+            return -1;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i <s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c == '}' && !stack.empty()){
+                if(stack.peek() == '{'){
+                    stack.pop();
+                }
+                else{
+                    stack.push(c);
+                }
+            }
+            else{
+                stack.push(c);
+            }
+        }
+
+        if(stack.size()%2 == 0){
+            return stack.size()/2 ;
+        }else{
+
+            return stack.size()/2+1;
+        }
+    }
+}
