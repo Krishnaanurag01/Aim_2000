eee5ecad2302adb643a59c67ef6cb507c837140a
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-24 22:26:54 +0530

    DSA Problems

---
+++ Searching_and_Sorting/Q15_Zero_Sum_Subarrays.java
@@ -0,0 +1,33 @@
+package Searching_and_Sorting;
+
+import java.util.HashMap;
+
+
+public class Q15_Zero_Sum_Subarrays {
+    public static void main(String[] args) {
+        
+    }
+    public static int getZeroSumSubarray(int[] arr) {
+       int i = -1;
+       HashMap<Integer,Integer> map = new HashMap<>() ; 
+       map.put(0, 1);
+
+       int sum = 0;
+       int ans= 0 ;
+
+       while (i < arr.length-1) {
+           i++;
+           sum+= arr[i];
+
+           if(map.containsKey(sum)){
+               ans += map.get(sum);
+               map.put(sum, map.get(sum)+1);
+           }
+           else{
+               map.put(sum, 1);
+           }
+       }
+     return ans;
+    }
+
+}
