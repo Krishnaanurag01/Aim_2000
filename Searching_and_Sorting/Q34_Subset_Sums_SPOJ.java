14f1ea4b0c6f4155339f7a761bde96681607d333
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-08-27 21:58:50 +0530

    DSA Problems

---
+++ Searching_and_Sorting/Q34_Subset_Sums_SPOJ.java
@@ -0,0 +1,30 @@
+package Searching_and_Sorting;
+
+public class Q34_Subset_Sums_SPOJ {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void getTotalSubsetSum(int[] arr , int A , int B) {
+        
+        int min = Integer.MAX_VALUE;
+        int sumValue = 0; 
+
+        for (int i : arr) {
+            min = Math.min(min, i);
+            sumValue += i;
+        }
+
+        int low = min ;
+        int ans = 0 ;
+        int high = sumValue;
+
+        while (low <= high) {
+
+            int mid = low + (high - low)/2;
+
+            if()
+            
+        }
+    }
+}
