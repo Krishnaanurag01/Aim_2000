ea53b9711db616fa4e023a5f5e0caf79dbc176b1
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-04 21:34:32 +0530

    DSA PROBLEMS

---
+++ Linked_List/Q35_delete_a_given_node.java
@@ -0,0 +1,11 @@
+package Linked_List ; 
+/**
+ * Q35_delete_a_given_node
+ */
+public class Q35_delete_a_given_node {
+
+    public static void main(String[] args) {
+        // node.val = node.next.val ;
+        // node.next = node.next.next ;
+    }
+}
