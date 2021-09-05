03ece3d57fc3df689c7a001b8e0b5d28e48ec59f
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-05 22:42:16 +0530

    DSA problems

---
+++ Binary_Tree/Q2_Reverse_Level_Order_Traversal.java
@@ -0,0 +1,78 @@
+package Binary_Tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+
+
+
+public class Q2_Reverse_Level_Order_Traversal {
+    
+    private TreeNode root ;
+
+    static class TreeNode {
+
+        int data ; 
+        TreeNode left ;
+        TreeNode right ;
+
+        public TreeNode(int d) {
+            data = d ;
+        }
+    }
+
+    public static ArrayList<Integer> get_reverse_level_traversal(TreeNode node) {
+
+        ArrayList<Integer> list = new ArrayList<>() ;
+        if(node == null){
+            return list ;
+        }
+
+        Queue <TreeNode> queue = new ArrayDeque<>();
+
+        queue.add(node) ;
+
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.peek() ;
+            list.add(temp.data);
+
+            if(temp.right != null) queue.add(temp.right);
+            if(temp.left != null) queue.add(temp.left);
+            
+            queue.remove() ;
+        }
+
+        Collections.reverse(list);
+
+        return list;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Q2_Reverse_Level_Order_Traversal bt = new Q2_Reverse_Level_Order_Traversal();
+
+        bt.root = new TreeNode(2);
+        bt.root.left = new TreeNode(3);
+        bt.root.right = new TreeNode(4);
+        bt.root.left.left = new TreeNode(5);
+        bt.root.left.right = new TreeNode(6);
+
+       System.out.println(get_reverse_level_traversal(bt.root));
+        
+    }
+}
