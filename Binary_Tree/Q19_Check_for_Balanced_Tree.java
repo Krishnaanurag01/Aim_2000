74a9eac6524e8ab7d590d5152336a36fb95daba9
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-08 21:50:33 +0530

    DSA PROBLEMS

---
+++ Binary_Tree/Q19_Check_for_Balanced_Tree.java
@@ -0,0 +1,63 @@
+package Binary_Tree;
+
+public class Q19_Check_for_Balanced_Tree {
+
+    private static TreeNode root ;
+
+    static class TreeNode {
+
+        int data ; 
+        TreeNode left ;
+        TreeNode right ;
+
+        public TreeNode(int d) {
+            data = d ;
+        }
+    }
+    
+
+
+    // time and space is o(n).
+
+
+    public static int isBalanced(TreeNode root) {
+        if(root == null) return 0  ;
+
+        int left = isBalanced(root.left) ; // move to left. if it has -1 then we have not balanced root in tree so simply return it.
+        if(left == -1) return -1;
+        int right = isBalanced(root.right) ; // same above.
+        if(right == -1) return -1;
+
+
+        if(Math.abs(left-right) > 1) return -1 ; // checking if not balanced.
+
+        return Math.max(left, right) + 1 ; // returning height.
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+             root = new TreeNode(20);
+        root.left = new TreeNode(8);
+        root.right = new TreeNode(22);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(3);
+        root.left.right.right = new TreeNode(14);
+        root.left.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(25);
+
+
+        System.out.println(isBalanced(root) != 1);
+    }
+    
+}
