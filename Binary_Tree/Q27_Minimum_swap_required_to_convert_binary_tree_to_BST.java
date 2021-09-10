4d641a8573b7de21f9a133dace2ccdf268e8c502
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-10 22:27:47 +0530

    DSA PROBLEMS

--- Binary_Tree/Q27_Minimum_swap_required_to_convert_binary_tree_to_BST.java
+++ Binary_Tree/Q27_Minimum_swap_required_to_convert_binary_tree_to_BST.java
@@ -1,6 +1,7 @@
 package Binary_Tree;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Q27_Minimum_swap_required_to_convert_binary_tree_to_BST {
@@ -15,53 +16,25 @@ public class Q27_Minimum_swap_required_to_convert_binary_tree_to_BST {
             data = d ;
         }
     }
+
+
+
+
+
+
+    // time and space is  o(n) in general.
+
     
-    private static void inorder(int[] arr, ArrayList<Integer> inordeList, int end , int index) {
-        if(index >= end) return ;
-
-        inorder(arr, inordeList, end, 2*index + 1);
-
-        inordeList.add(arr[index]);
-
-        inorder(arr, inordeList, end, 2*index + 2);
-    }
-
-    public static int get_min_swaps_to_bst(int[] arr ) {
-        ArrayList<Integer> inordeList = new ArrayList<>();
-        inorder(arr,inordeList,arr.length-1 , 0);
+    // The idea is to use the fact that inorder traversal of Binary Search Tree is in increasing order of their value. 
+// So, find the inorder traversal of the Binary Tree and store it in the array and try to sort the array. The minimum number of swap required to get the array sorted will be the answer
 
 
-        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], i);
-        }
 
-        int swaps = 0 ;
+    
+    // doing the same thing as Q18 of Searching and sorting.
 
-        for (int i = 0; i < arr.length; i++) {
-
-            if(arr[i] != inordeList.get(i)){
-
-                swaps++;
-
-                int originalValue = arr[i];
-
-                swap(arr,i,map.get(inordeList.get(i)));
-
-                map.put(originalValue,map.get(inordeList.get(i)));
-
-                map.put(inordeList.get(i), i);
-
-            }
-            
-        }
-
-        return swaps ;
-
-    }
-
-    private static void swap(int[] arr, int i, int swappingIdx) {
+    private static void swap(int[] arr, int i, int swappingIdx) { // its simple swap function.
 
         int temp = arr[i];
         arr[i] = arr[swappingIdx];
@@ -69,8 +42,55 @@ public class Q27_Minimum_swap_required_to_convert_binary_tree_to_BST {
     }
 
 
+    public static void inorder2(int[] arr , ArrayList<Integer> list , int size , int index) { // here took arraylist not array because it will not be possible to store value in array without any index.
+        if(index >= size ) return ;
+
+        inorder2(arr, list, size, 2*index + 1); /// here 2*index  + 1 denotes the left child which in on (2*index  + 1)index.
+        list.add(arr[index]); // storing the inorder to llist.
+        inorder2(arr, list, size, 2*index + 2); // 2*index + 2 is right child.
+
+    }
+
+
+    public static int min_swap_for_bst(int[] arr) {
+     
+
+        ArrayList<Integer> list = new ArrayList<>(); // for finding the inorder.
+        inorder2(arr, list, arr.length-1 ,0);
+
+        int[] inOrder = list.stream().mapToInt(i -> i).toArray();  // converting the list into array.
+        list.clear(); // removed the nodes of list. as its of no use.
+
+        HashMap<Integer,Integer> map = new HashMap<>() ; // for storing the original index of inorder.
+
+        for (int i = 0; i < inOrder.length; i++) {
+            map.put(inOrder[i], i); // we have to sort the inorder so we'll store its original value.
+        }
+
+        Arrays.sort(arr); // now sorting the given array.
+        int ans = 0;// total swaps.
+
+        for (int i = 0; i < inOrder.length; i++) { // rest is same.
+            if(arr[i] != inOrder[i]){
+
+                ans++;
+
+                int originalVal = inOrder[i];
+
+                swap(inOrder, i, map.get(arr[i]));
+
+                map.put(originalVal, map.get(arr[i]));
+                map.put(arr[i], i) ;
+
+            }
+        }
+        return ans ;
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println(get_min_swaps_to_bst(new int[]{ 5, 6, 7, 8, 9, 10, 11 }));
+        // System.out.println(get_min_swaps_to_bst(new int[]{ 5, 6, 7, 8, 9, 10, 11 }));
+        System.out.println(min_swap_for_bst(new int[]{ 5, 6, 7, 8, 9, 10, 11 }));
     }
 
 
