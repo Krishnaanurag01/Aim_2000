7114c4b93ebafd4c73cede85a411fb0e20a77ce4
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-14 22:59:21 +0530

    DSA PROBLEMS

--- Strings/Q21_Count_Palindromic_Subsequences.java
+++ Strings/Q21_Count_Palindromic_Subsequences.java
@@ -7,34 +7,34 @@ public class Q21_Count_Palindromic_Subsequences {
      System.out.println(countPalindromicSubsequence("aab"));
     }
 
-    public static int countPalindromicSubsequence(String str) {
+    public static long countPalindromicSubsequence(String str) {
     
 
-        int[][] dp = new int[str.length()][str.length()];
+        long[][] dp = new long[str.length()][str.length()];
 
-        for(int g = 0 ; g < str.length() ; g++){
+        for(int g = 0 ; g < dp.length ; g++){
 
-            for(int i = 0 , j = g ; j < str.length() ; i++, j++ ){
+            for(int i = 0 , j = g ; j < dp[0].length ; i++, j++ ){
 
-                if(g ==0 ){
+                if(g == 0 ){
                     dp[i][j] = 1;
                 }
-                else if(g ==1){
+                else if(g == 1){
 
                     dp[i][j] = str.charAt(i) == str.charAt(j) ? 3 : 2 ;
                 }
                 else{
                     if(str.charAt(i) == str.charAt(j)){
-                        dp[i][j] = dp[i][j-1] + dp[i+1][j] + 1 ; 
+                        dp[i][j] = dp[i][j - 1] + dp[i + 1][j] + 1 ; 
                     }
                     else{
-                        dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1];
+                        dp[i][j] = dp[i][j-1] + dp[i + 1][j] - dp[i+1][j-1];
                     }
                 }
             }
         }
 
-        return dp[0][str.length()-1] ;
+        return dp[0][dp[0].length-1] ;
     }
 
     }
