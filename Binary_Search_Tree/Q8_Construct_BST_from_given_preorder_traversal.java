71cfa78383d3cad3d65b18ea6d88211f33d60815
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-15 22:32:54 +0530

    DSA PROBLEMS

---
+++ Binary_Search_Tree/Q8_Construct_BST_from_given_preorder_traversal.java
@@ -0,0 +1,80 @@
+package Binary_Search_Tree;
+
+
+public class Q8_Construct_BST_from_given_preorder_traversal {
+    
+    Node root;
+
+    public static class Node {
+
+        int data;
+        Node left, right;
+        public Node(int d) {
+            this.data = d;
+        }
+
+    }
+
+    public void insert_node(int data) {
+        root = insert_node(root, data);
+    }
+
+    public static Node insert_node(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (root.data > data) {
+            root.left = insert_node(root.left, data);
+        } else {
+            root.right = insert_node(root.right, data);
+        }
+
+        return root;
+    }
+
+
+    public static void preOrder(Node root) {
+        if(root == null) return ;
+
+        System.out.print(root.data +" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static Node constructTree(int[] pre) { 
+        if(pre.length == 0) return null ;
+
+        int left_range = - (int) 1e9 - 1 ;  // take left range as - infinit (min integer value - 1) 
+        int right_range = (int) 1e9 + 1 ;   // take right range as infinit (max integer value + 1)
+
+        return constructTree(pre,left_range , right_range); // now construct with it.
+    }
+
+
+    static int index = 0 ;
+    private static Node constructTree(int[] pre, int left_range, int right_range) {
+          // when index is greater or equal  then we performed for  all the nodes so return it. 
+          // otherwise when arr[index] is less than or greater than left and right range then return.
+        if(index >= pre.length || pre[index] < left_range || pre[index] > right_range){
+            return null ;
+        }
+        Node node = new Node(pre[index++]) ;  // otherwise make a node .
+        node.left = constructTree(pre,left_range , node.data);  // perform same with different range in left.
+        node.right = constructTree(pre, node.data  ,right_range) ; // and different for right .
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+
+        int pre[] = new int[] { 10, 5, 1, 7, 40, 50 };
+
+        Node root = constructTree(pre) ;
+
+        preOrder(root);
+        
+    }
+}
