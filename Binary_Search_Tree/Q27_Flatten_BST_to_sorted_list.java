f425deda23406ff279566c35a902166334512a87
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-19 22:32:50 +0530

    DSA PROBLEMS

--- Binary_Search_Tree/Q27_Flatten_BST_to_sorted_list.java
+++ Binary_Search_Tree/Q27_Flatten_BST_to_sorted_list.java
@@ -3,13 +3,13 @@ package Binary_Search_Tree;
 public class Q27_Flatten_BST_to_sorted_list {
     
     
-    Node root;
+    TreeNode root;
 
-    public static class Node {
+    public static class TreeNode {
 
         int data;
-        Node left, right;
-        public Node(int d) {
+        TreeNode left, right;
+        public TreeNode(int d) {
             this.data = d;
         }
 
@@ -20,9 +20,9 @@ public class Q27_Flatten_BST_to_sorted_list {
         root = insert_node(root, data);
     }
 
-    public static Node insert_node(Node root, int data) {
+    public static TreeNode insert_node(TreeNode root, int data) {
         if (root == null) {
-            root = new Node(data);
+            root = new TreeNode(data);
             return root;
         }
 
@@ -36,19 +36,49 @@ public class Q27_Flatten_BST_to_sorted_list {
     }
 
 
-    public static void flattening_bst_to_list(Node root,Node prev) {
+    // method : 01
+    // time and space is : o(n) 
+
+    static TreeNode prev = null ;
+
+    public static void flattening_bst_to_list(TreeNode root) {
         if(root == null ) return ;
 
-        flattening_bst_to_list(root.left, prev);
+        flattening_bst_to_list(root.right);
+        flattening_bst_to_list(root.left);
 
-        prev.right = root;
+       root.right = prev;
         root.left = null;
         prev = root ;
 
-        flattening_bst_to_list(root.right, prev);
 
     }
 
+
+    // method : 03 
+    // time : o(n) and space is o(1).
+
+    public static void bst_to_list_constant_space(TreeNode root) {
+        TreeNode current = root ;
+
+        while (current != null) {
+            
+            if(current.left != null){
+
+                TreeNode right_max = current.left ;
+
+                while (right_max.right != null) {
+                    right_max = right_max.right ;
+                }
+
+                right_max.right = current.right ;
+                current.right = current.left ;
+                current.left = null ;
+            }
+
+            current = current.right ;
+        }
+    }
 
 
 
