df16415e34c1af69cad8b2ac064f745f6621a8bb
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-26 22:19:30 +0530

    DSA PROBLEMS

--- Dynamic_Programming_level_02/Q6_Print_All_Minimum_Cost_Paths.java
+++ Dynamic_Programming_level_02/Q6_Print_All_Minimum_Cost_Paths.java
@@ -21,6 +21,8 @@ public class Q6_Print_All_Minimum_Cost_Paths {
 
     public static void print_min_path_cost(int[][] path) {
         
+
+        // finding minimum path which we already done in level - 1.
         int n = path.length ;
         int m = path[0].length ;
 
@@ -46,35 +48,41 @@ public class Q6_Print_All_Minimum_Cost_Paths {
             }
         }
 
+
+        // printing the minimum path cost.
         System.out.println(dp[0][0]);
 
+        /// making a queue for level order traversal.
         ArrayDeque<pair> queue = new ArrayDeque<>() ;
 
-        queue.add(new pair(0, 0, "")) ;
+        queue.add(new pair(0, 0, "")) ; // making a pair of i = 0 ; j = 0 ; and empty string path.
 
 
         while (queue.size() > 0) {
             pair rp = queue.removeFirst() ;
+            // removing first.
 
-            if(rp.i == n-1 && rp.j == m-1) {
+            // doing the same conditons which we done at the finding the minimum cost.
+
+            if(rp.i == n-1 && rp.j == m-1) { // checking if the removed pair is the bottom right node. then this is our answer so print it.
                 System.out.println(rp.path);
             }
-            else if(rp.i == n-1)
+            else if(rp.i == n-1) // if last row then go horizontally.
             {
                 queue.add( new pair(rp.i, rp.j+1 , rp.path+"H"));
             }
-            else if(rp.j == m-1){
+            else if(rp.j == m-1){ // if last col then go below vertically.
                 queue.add( new pair(rp.i+1, rp.j , rp.path+"V"));
             }
-            else{
-                if(dp[rp.i][rp.j+1] < dp[rp.i+1][rp.j]){
+            else{ // comparing the path between same row next col and next row same col.
+                if(dp[rp.i][rp.j+1] < dp[rp.i+1][rp.j]){ // if next col is lower then add.
 
                     queue.add( new pair(rp.i, rp.j+1 , rp.path+"H"));
                 }
-                else if(dp[rp.i][rp.j+1] > dp[rp.i+1][rp.j]){
+                else if(dp[rp.i][rp.j+1] > dp[rp.i+1][rp.j]){ /// if below path is lower then add it.
                     queue.add( new pair(rp.i+1, rp.j , rp.path+"V"));
                 }
-                else{
+                else{ // if both paths are same then add both.
                     queue.add( new pair(rp.i+1, rp.j , rp.path+"V"));
                     queue.add( new pair(rp.i, rp.j+1 , rp.path+"H"));
                 }
