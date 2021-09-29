5b118a0534ef8961e32ac71a708d9e2bb4585239
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-29 22:30:54 +0530

    DSA PROBLEMS

--- Strings/Q29_Find_the_longest_common_subsequence_between_two_strings.java
+++ Strings/Q29_Find_the_longest_common_subsequence_between_two_strings.java
@@ -7,18 +7,16 @@ public class Q29_Find_the_longest_common_subsequence_between_two_strings {
 
     public static int getLCS(int x , int y , String s1 , String s2) {
         
-        int dp[][] = new int[s1.length() +1 ][s2.length() + 1];
+        int dp[][] = new int[s1.length() + 1 ][s2.length() + 1];
         for (int i = dp.length - 2 ; i >=0 ; i--) {
-            for (int j = dp[0].length -2  ; j >=0 ; j--){
+            for (int j = dp[0].length - 2 ; j >=0 ; j--){
 
-                if (s1.charAt(i)== s1.charAt(j)) {
-                    dp[i][j] = 1 +  dp[i+1][j+1];
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = 1 +  dp[i + 1][j + 1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i+1][j] , dp[i][j+1]);
+                    dp[i][j] = Math.max(dp[i + 1][j] , dp[i][j + 1]);
                 }
-
-
             }
         }
 
