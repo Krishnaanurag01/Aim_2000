257aa80b262be752b9e3a9749323d8e317817806
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-09-29 22:29:35 +0530

    DSA PROBLEMS

---
+++ Dynamic_Programming_level_02/Q24_Dyck_Path.java
@@ -0,0 +1,27 @@
+package Dynamic_Programming_level_02;
+
+public class Q24_Dyck_Path {
+    
+
+    // using catalan numbers method.
+    static Long dyckPaths(int N) {
+        
+        long[] dp = new long[N+1];
+        dp[0] = 1 ;
+        dp[1] = 1 ;
+        
+        for(int i = 2 ; i <= N ; i++){
+            
+            int l = i-1 ;
+            int r = 0 ;
+            
+            while(l >= 0){
+                dp[i] += dp[l]*dp[r] ;
+                l-- ;
+                r++;
+            }
+        }
+        
+        return dp[N]; 
+    }
+}
