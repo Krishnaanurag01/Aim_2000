cd21708ffbda5e7a27f6de458e2746d3bfe23413
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-10-03 08:46:34 +0530

    DSA PROBLEMS

--- Dynamic_Programming_level_02/Q27_Matrix_Chain_Multiplication.java
+++ Dynamic_Programming_level_02/Q27_Matrix_Chain_Multiplication.java
@@ -10,26 +10,30 @@ public class Q27_Matrix_Chain_Multiplication {
 
         int n = arr.length ;
 
+        // making arr.length - 1 dp as arr contains last index -1 and last index of array is n*m of last matrix. so last index is nothing but dimention of last matrix.
         int[][] dp = new int[n-1][n-1] ;
 
-        for (int g = 0; g < dp.length; g++) {
+        for (int g = 0; g < dp.length; g++) { // using gap strategy . 
+            // i is row and j is column.
             for (int i = 0 , j = g ; j < dp.length; i++,j++) {
                 
-                if(g == 0){
+                if(g == 0){ // when g = 0 then it means we have only one matrix so there's no need of multiplication.
                     dp[i][j] = 0 ;
                 }
-                else if(g == 1){
+                else if(g == 1){ // if g = 1 then multiply first matrix(arr[i]) with second matrix (arr[j]).
+                    // here arr[i] is r of first matrix and arr[j] is r of second matrix and arr[j+1] is m of second matrix.
+                    // so formula is : r(first matrix row)*r(second matrix row)*m(second matrix column).
                     dp[i][j] = arr[i] * arr[j] * arr[j+1] ;
                 }
                 else{
 
-                    int min = Integer.MAX_VALUE ;
+                    int min = Integer.MAX_VALUE ; // else take a min.
 
-                    for (int k = i ; k < j ; k++) {
+                    for (int k = i ; k < j ; k++) { // now start from i to j - 1 
                         
-                        int l = dp[i][k] ;
-                        int r = dp[k+1][j] ;
-                        int m = arr[i] * arr[k+1] * arr[j+1] ;
+                        int l = dp[i][k] ; // left is i to k
+                        int r = dp[k+1][j] ; // right is k+1 to j
+                        int m = arr[i] * arr[k+1] * arr[j+1] ; //so formula is : r(first matrix row)*r(second matrix row)*m(second matrix column).
                         int total = l + r + m ;
                         if( total < min ){
                             min = total ;
