90e3ed9c2a3d172fba461059a6a981f0458c9d02
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-10-14 22:44:20 +0530

    DSA PROBLEMS

--- Dynamic_Programming_level_01/Q4_Climbing_Stairs_with_Jumps.java
+++ Dynamic_Programming_level_01/Q4_Climbing_Stairs_with_Jumps.java
@@ -23,7 +23,7 @@ public class Q4_Climbing_Stairs_with_Jumps {
     public static int climbing_stairs_with_jump(int[] jumps , int n ) {
         int[] dp = new int[n + 1] ; // for storing data.
 
-        dp[n] = 1 ; // as we have to go to the nth stair , so dp[n] = 1 .
+        dp[n] = 1 ; // as we have to go to the end from nth stair , so dp[n] = 1 .
 
         for (int i = n-1 ; i >= 0 ; i--) { // we'll start from last as min number of stairs takes to reach at the nth stair.
             for (int j = 0; j <= jumps[i] && i+j < dp.length ; j++) { // look for given array about where can we jump from current location and the location ( i + j ) should be under dp region. (o\w give out of bound error)
