3b3205e497f41f742c1212533d23e48c0ff804cc
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-10-14 22:43:58 +0530

    DSA PROBLEMS

---
+++ Dynamic_Programming_level_02/Q78_Painting_the_Fence.java
@@ -0,0 +1,29 @@
+package Dynamic_Programming_level_02;
+
+public class Q78_Painting_the_Fence {
+
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static int get_total_ways_to_paint(int n , int k) {
+        
+        if(n==1){
+            return k ;
+        }
+
+        int last_same = k ; // total k colours can be same at last two fence
+        int last_diff = k*(k-1) ; // and this is total of last different , here k * (k-1) denotes : current colour with different(k-1) colours can be paired.
+        int total = last_diff + last_same ;
+
+        for (int i = 3 ; i <= n ; i++) {
+            last_same = last_diff ;
+            last_diff = total*(k-1) ;
+            total = last_diff + last_same ;
+        }
+
+        return total ; 
+    }
+    
+}
