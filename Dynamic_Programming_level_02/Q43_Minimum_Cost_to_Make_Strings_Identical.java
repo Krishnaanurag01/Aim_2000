e8f02a4bc3011c7f85b2b12a895a42f6dfd426ef
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-10-17 22:29:15 +0530

    DSA PROBLEMS

--- Dynamic_Programming_level_02/Q43_Minimum_Cost_to_Make_Strings_Identical.java
+++ Dynamic_Programming_level_02/Q43_Minimum_Cost_to_Make_Strings_Identical.java
@@ -13,26 +13,26 @@ public class Q43_Minimum_Cost_to_Make_Strings_Identical {
         
         int[][] dp = new int[s1.length()+1][s2.length()+1] ;
 
-        for (int i = dp.length - 1; i >= 0 ; i--) {
-            for (int j = dp[0].length - 1; j >= 0 ; j--) {
+        for (int i = dp.length - 1; i >= 0 ; i--) {  // starting from dp last row
+            for (int j = dp[0].length - 1; j >= 0 ; j--) { // dp last col
                 
-                if(i == dp.length-1 || j == dp[0].length){
+                if(i == dp.length-1 || j == dp[0].length-1 ){ // last row and col is 0 
                     dp[i][j] = 0 ;
                 }
                 else{
-                    if(s1.charAt(i) == s2.charAt(j)){
+                    if(s1.charAt(i) == s2.charAt(j)){ // if char same then add 1 with diagonal
                         dp[i][j] = 1 + dp[i+1][j+1] ;
                     }
                     else{
-                        dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]) ;
+                        dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]) ;  // else store max of below and right
                     }
                 }
             }
         }
 
-        int lcs = dp[0][0] ;
+        int lcs = dp[0][0] ; 
 
-        int cost_in_s1 = (s1.length() - lcs)*cost1 ;
+        int cost_in_s1 = (s1.length() - lcs)*cost1 ; // subtract from s1 length and multiplying with its cost and same for above
         int cost_in_s2 = (s2.length() - lcs)*cost2 ;
 
         int ans = cost_in_s1 + cost_in_s2 ;
