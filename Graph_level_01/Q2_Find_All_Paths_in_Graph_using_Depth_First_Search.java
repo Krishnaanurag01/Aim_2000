6ac1b10e2ed834648ffbeb074f6613ddb6072aee
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-11-01 22:40:52 +0530

    DSA PROBLEMS

--- Graph_level_01/Q2_Find_All_Paths_in_Graph_using_Depth_First_Search.java
+++ Graph_level_01/Q2_Find_All_Paths_in_Graph_using_Depth_First_Search.java
@@ -52,6 +52,7 @@ public class Q2_Find_All_Paths_in_Graph_using_Depth_First_Search {
         
     }
 
+    // almost same as q1
     public static void print_all_paths(ArrayList<Edge>[] graph , int source , int dest , boolean[] visited , String psf) {
         
         if( source == dest ) {
