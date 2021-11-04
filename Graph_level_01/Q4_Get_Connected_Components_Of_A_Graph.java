4b7e3e98d4364caaeca2dbfb50e3b525184a44e1
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-11-04 15:40:20 +0530

    DSA PROBLEMS

--- Graph_level_01/Q4_Get_Connected_Components_Of_A_Graph.java
+++ Graph_level_01/Q4_Get_Connected_Components_Of_A_Graph.java
@@ -38,15 +38,15 @@ public class Q4_Get_Connected_Components_Of_A_Graph {
             graph[v2].add(new Edge(v2, v1, wt)) ;
         }
         
-        boolean[] visited = new boolean[vert] ;
+        boolean[] visited = new boolean[vert] ; // made an boolean array which will keep track of element who are already visited.
 
-        ArrayList<ArrayList<Integer>> comp = new ArrayList<>() ;
+        ArrayList<ArrayList<Integer>> comp = new ArrayList<>() ; // this will store the arraylist of paths.
 
         for (int i = 0; i < visited.length; i++) {
-            if(visited[i] == false){
+            if(visited[i] == false){ // if element is not visited then make an arraylist which will store the paths.
                 ArrayList<Integer> inner_comp = new ArrayList<>() ;
-                get_component(graph,i,visited,inner_comp) ;
-                comp.add(inner_comp) ;
+                get_component(graph,i,visited,inner_comp) ; // send it with inner comp
+                comp.add(inner_comp) ; // now adding the inner comp into the main list.
             } 
         }
 
@@ -56,11 +56,11 @@ public class Q4_Get_Connected_Components_Of_A_Graph {
     private static void get_component(ArrayList<Edge>[] graph, int src, boolean[] visited,
             ArrayList<Integer> inner_comp) {
 
-                visited[src] = true ;
-                inner_comp.add(src) ;
+                visited[src] = true ; // making visited true.
+                inner_comp.add(src) ; // adding to the comp
 
                 for (Edge e : graph[src]) {
-                    if( visited[ e.nbr ] != true){
+                    if( visited[ e.nbr ] != true){ // checking if neigbour is not visited then run the function with src == neigbour. 
                         get_component(graph, e.nbr , visited, inner_comp);
                     }
                 }
