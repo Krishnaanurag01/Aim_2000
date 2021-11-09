7c502ccc8988663f2c895c209214d19da7feaf8e
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-11-09 22:30:10 +0530

    DSA PROBLEMS

---
+++ Graph_level_01/Detect_cycle_in_a_directed_graph.java
@@ -0,0 +1,41 @@
+package Graph_level_01;
+import java.util.*;
+
+public class Detect_cycle_in_a_directed_graph {
+    
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+
+        boolean[] visited = new boolean[V];
+        boolean[] Dfs_visited = new boolean[V];
+
+        for (int i = 0; i < V ; i++) {
+            
+            if( visited[i] == false ){
+
+                if( check_cycle(adj,visited,Dfs_visited,i)){
+                    return true ;
+                }
+            }
+        }
+
+        return false ;
+    }
+
+    private boolean check_cycle(ArrayList<ArrayList<Integer>> adj, boolean[] visited, boolean[] dfs_visited, int i) {
+
+        visited[i] = true ;
+        dfs_visited[i] = true ;
+
+        for (Integer in : adj.get(i)) {
+            if(visited[in] == false){
+                if(check_cycle(adj, visited, dfs_visited, in)) return true ;
+            }
+            else if(dfs_visited[i] == true){
+                return true ;
+            }
+        }
+
+        return false;
+    }
+}
