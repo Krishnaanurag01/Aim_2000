db810b18f0cb9d7be44adbe7ac5600960cfe9b64
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-11-11 22:31:12 +0530

    DSA PROBLEMS

---
+++ Problem_Of_The_day/Q4_Array_Subset_of_another_array.java
@@ -0,0 +1,33 @@
+package Problem_Of_The_day;
+
+import java.util.HashSet;
+
+public class Q4_Array_Subset_of_another_array {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+    public static String is_subset(long[] a1 , long[] a2 , long n , long m) {
+        
+        HashSet<Long> set = new HashSet<>() ;
+
+        for (int i = 0; i < a1.length; i++) {
+            set.add(a1[i]) ;
+        }
+
+
+        String is_subset_huh = "No" ;
+
+        for (int i = 0; i < a2.length; i++) {
+            if(set.contains(a2[i]) == false){
+                is_subset_huh = "Yes";
+                break ;
+            }
+        }
+
+        return is_subset_huh ;
+    }
+    
+}
