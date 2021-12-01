86fdceca0e77ffe33c8b8427353a5cebfc638055
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-01 23:03:39 +0530

    DSA PROBLEM

--- Linked_List/Q2_Reverse_a_Linked_List_in_groups_of_given_size.java
+++ Linked_List/Q2_Reverse_a_Linked_List_in_groups_of_given_size.java
@@ -5,27 +5,27 @@ package Linked_List;
 public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
 
 
-   static node head ;
+   static Node head ;
 
    Q2_Reverse_a_Linked_List_in_groups_of_given_size(){
        head = null;
    }
 
-    public class node {
+    public class Node {
 
         int data ;
-        node next; 
+        Node next; 
 
-        public node(int d){
+        public Node(int d){
             data = d ;
             next = null ;
         }        
     }
 
 
-    public void printLinkedList(node head) {
+    public void printLinkedList(Node head) {
 
-        node printIt = head;
+        Node printIt = head;
         while(printIt != null){
             System.out.print(printIt.data + " -> ");
             printIt = printIt.next;
@@ -35,13 +35,13 @@ public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
     }
 
     public  void insertNode(int d) {
-        node newNode = new node(d);
+        Node newNode = new Node(d);
         if(head == null){
             head = newNode ;
             return;
         }
 
-        node temp = head;
+        Node temp = head;
 
         while (temp.next != null) {
             temp = temp.next;            
@@ -61,11 +61,11 @@ public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
 
     // this will return length .
 
-    public static int length(node head){
+    public static int length(Node head){
 
         int len = 0 ;
 
-        node temp = head ;
+        Node temp = head ;
 
         while (temp != null) {
             temp = temp.next ;
@@ -76,40 +76,40 @@ public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
         return len ;
     }
 
-    static node tempHead = null ;
-    static node tempTail = null ;
+    static Node tempHead = null ;
+    static Node tempTail = null ;
 
     // this function add nodes in the start.
 
-    public static void addFirst(node d) {
+    public static void addFirst(Node d) {
         if(tempHead == null){
             tempHead = d ;
             tempTail = d ;
         }
         else{
             d.next = tempHead; // this will connect previous list with our new nodes next.
-            tempHead = d; // and then our new node will become temp head.
+            tempHead = d; // and then our new Node will become temp head.
         }
     }
 
-    // time complexcity is : o(n) and space is o(1). here we used two while  together but still complexcity is o(n) becuase first while loop runs for very less time and by the end we only access all the n node only.
-    public static node reverseIt_in_K_Group(node head , int k){
+    // time complexcity is : o(n) and space is o(1). here we used two while  together but still complexcity is o(n) becuase first while loop runs for very less time and by the end we only access all the n Node only.
+    public static Node reverseIt_in_K_Group(Node head , int k){
 
         if( head == null || head.next == null || k == 0){
             return head; // when empty or k is zero then we don't have to do anything.
         }
 
-        node originalHead = null , originalTail = null ; // this will be our original head pointer which we return in the end.
+        Node originalHead = null , originalTail = null ; // this will be our original head pointer which we return in the end.
 
         int length = length(head); // first finding the length of ll. 
 
-        node current = head; // now creating a temp node and copying the linked list.
+        Node current = head; // now creating a temp Node and copying the linked list.
 
         while(length >= k){  // while length is greater or equal to k perform it.
             int tempK = k; // taking k in a temp variable becoz we have to perform k times loop for every group. 
 
             while (tempK-- > 0) {
-                node forward = current.next;  // this will link the next nodes.
+                Node forward = current.next;  // this will link the next nodes.
                 current.next = null; 
                 addFirst(current);  // adding it to the start of tempHead.
                 current = forward ; // now we get the rest nodes here.
@@ -120,7 +120,7 @@ public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
                 originalTail = tempTail ;
             }
             else{
-                originalTail.next = tempHead ; // when we already have orignal head and next then we don't have to touch head we just upate the original temp. this will link the new temp head with original tail.
+                originalTail.next = tempHead ; // when we already have orignal head and next then we don't have to touch head we just update the original temp. this will link the new temp head with original tail.
                 originalTail = tempTail ;  // and then we make our new temp head to original tail.
             }
 
@@ -132,7 +132,7 @@ public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
 
         // now as questions says , we have to reverse rest of the nodes which are lesser than k. so we again do the same thing till we reach the end.
         while (current != null) {
-            node forward = current.next;
+            Node forward = current.next;
             current.next = null;
             addFirst(current);
             current = forward ;
@@ -180,9 +180,9 @@ public class Q2_Reverse_a_Linked_List_in_groups_of_given_size {
 
         /// for this you have to make 3 function and 2 static varibale i.e 
         // length function , add to first function , and reversein in k group function.
-        // temphead and temptail node.
+        // temphead and temptail Node.
 
-      node ansHead =  reverseIt_in_K_Group(head , 3);
+      Node ansHead =  reverseIt_in_K_Group(head , 3);
       ll.printLinkedList(ansHead);
     }
 
