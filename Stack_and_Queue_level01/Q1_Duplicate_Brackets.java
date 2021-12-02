56f04204608d4d6c00f7f0a76033147f33878e64
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-02 23:05:58 +0530

    DSA PROBLEM

---
+++ Stack_and_Queue_level01/Q1_Duplicate_Brackets.java
@@ -0,0 +1,39 @@
+package Stack_and_Queue_level01;
+
+import java.util.Stack;
+
+public class Q1_Duplicate_Brackets {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean hasDuplicate(String expr) { /// (a + b) + ((c + d))
+        
+        Stack<Character> stack = new Stack<>() ;
+
+        for (int i = 0; i < expr.length() ; i++) {
+            
+            char ch = expr.charAt(i) ;
+
+            if(ch == ')'){ // when closing bracket appears.
+                if(stack.peek() == '('){ // check if stack has opening one then it means duplicacy is here
+                    return true ;
+                }else{
+                    while(stack.size() > 0 ){ // otherwise pop until you get the opening side
+                        char ch2 = stack.pop() ;
+                        if(ch2 == '('){
+                            break ;
+                        }
+                    }
+                }
+            }
+            else{ // addding to the stack
+                stack.push(ch) ;
+            }
+        }
+
+
+
+        return false ;
+    }
+}
