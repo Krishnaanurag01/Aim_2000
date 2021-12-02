62f211abbd5aedb97bfe5b4c7a9e4851efcf025a
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-02 23:07:01 +0530

    DSA PROBLEM

--- Stack_and_Queue_level01/Q2_Balanced_Brackets.java
+++ Stack_and_Queue_level01/Q2_Balanced_Brackets.java
@@ -7,60 +7,60 @@ public class Q2_Balanced_Brackets {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            String str = sc.nextLine();
 
-        String str = sc.nextLine();
+            Stack<Character> stack = new Stack<>();
 
-        Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < str.length(); i++) {
 
-        for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
 
-            char ch = str.charAt(i);
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    stack.push(ch);
+                } else {
+                    if (stack.size() > 0) {
 
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } else {
-                if (stack.size() > 0) {
+                        if (ch == ')') {
+                            char ch2 = stack.pop();
+                            if (ch2 != '(') {
+                                System.out.println(false);
+                                return;
+                            }
+                        } else if (ch == '}') {
 
-                    if (ch == ')') {
-                        char ch2 = stack.pop();
-                        if (ch2 != '(') {
-                            System.out.println(false);
-                            return;
-                        }
-                    } else if (ch == '}') {
+                            char ch2 = stack.pop();
+                            if (ch2 != '{') {
+                                System.out.println(false);
+                                return;
+                            }
 
-                        char ch2 = stack.pop();
-                        if (ch2 != '{') {
-                            System.out.println(false);
-                            return;
-                        }
+                        } else if (ch == ']') {
 
-                    } else if (ch == ']') {
-
-                        char ch2 = stack.pop();
-                        if (ch2 != '[') {
-                            System.out.println(false);
-                            return;
+                            char ch2 = stack.pop();
+                            if (ch2 != '[') {
+                                System.out.println(false);
+                                return;
+                            }
                         }
                     }
-                }
-                else if(ch == ')' || ch == '}' || ch == ']'){
-                    System.out.println(false);
-                    return ;
+                    else if(ch == ')' || ch == '}' || ch == ']'){
+                        System.out.println(false);
+                        return ;
+                    }
                 }
             }
-        }
 
-        if (stack.size() > 0) {
-            System.out.println(false);
-        } else {
-            System.out.println(true);
-        }
-
+            if (stack.size() > 0) {
+                System.out.println(false);
+            } else {
+                System.out.println(true);
+            }
 
 
-        sc.close(); 
+
+            sc.close();
+        } 
 
     }
 }
