6345e62b22f438817bbe37cb91734510504f3e40
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-02 23:06:29 +0530

    DSA PROBLEM

---
+++ Stack_and_Queue_level01/Q5_Stock_span_problem.java
@@ -0,0 +1,36 @@
+package Stack_and_Queue_level01;
+
+import java.util.Stack;
+
+public class Q5_Stock_span_problem {
+
+    public static int[] calculateSpan(int price[], int n)
+    {
+
+        Stack<Integer> stack = new Stack<>() ;
+        int[] ans = new int[n] ;
+
+        ans[0] = 1 ;
+        stack.add(0) ;
+        
+        for(int i = 1 ; i < n ; i++){
+            
+            while(stack.size() > 0 && price[i] >= price[stack.peek()] ){
+                stack.pop() ;
+            }
+            
+            if(stack.size() == 0){
+                ans[i] = i + 1 ;
+            }
+            else{
+                ans[i] = i - stack.peek() ;
+            }
+            
+            stack.push(i) ;
+        }
+
+      
+        return ans ;
+    }
+    
+}
