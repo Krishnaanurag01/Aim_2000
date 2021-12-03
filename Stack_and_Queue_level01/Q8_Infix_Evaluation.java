a86365394c954f62e38913f3c0127f886740d777
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-03 22:52:55 +0530

    DSA PROBLEM

--- Stack_and_Queue_level01/Q8_Infix_Evaluation.java
+++ Stack_and_Queue_level01/Q8_Infix_Evaluation.java
@@ -7,64 +7,64 @@ public class Q8_Infix_Evaluation {
 
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner( System.in ) ;
+        try (Scanner sc = new Scanner( System.in )) {
+            String str = sc.nextLine() ;
 
-        String str = sc.nextLine() ;
+            Stack<Integer> oprnd = new Stack<>() ;
+            Stack<Character> oprtor = new Stack<>() ;
 
-        Stack<Integer> oprnd = new Stack<>() ;
-        Stack<Character> oprtor = new Stack<>() ;
+            for (int i = 0; i < str.length() ; i++) {
+                char ch = str.charAt(i) ;
 
-        for (int i = 0; i < str.length() ; i++) {
-            char ch = str.charAt(i) ;
-
-            if (ch == '(' ) {
-                oprtor.push(ch) ;
-            }
-            else if( Character.isDigit(ch)){
-                oprnd.push(ch - '0') ;
-            }
-            else if(ch == ')'){
-
-                while (oprtor.size() > 0 && oprtor.peek() != '(') {
-                    
-                    int v2 = oprnd.pop() ;
-                    int v1 = oprnd.pop() ;
-                    char opration = oprtor.pop() ;
-
-                    int result = operations(v1, v2, opration) ;
-                    oprnd.push(result) ;
+                if (ch == '(' ) {
+                    oprtor.push(ch) ;
                 }
-
-                oprtor.pop() ; // removing opening bracket
-
-            }
-            else if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
-
-                while (oprtor.size() > 0 && oprtor.peek() != '(' && precedance(oprtor.peek()) >= precedance(ch) ) {
-                    
-                    int v2 = oprnd.pop() ;
-                    int v1 = oprnd.pop() ;
-                    char opration = oprtor.pop() ;
-
-                    int result = operations(v1, v2, opration) ;
-                    oprnd.push(result) ;
+                else if( Character.isDigit(ch)){
+                    oprnd.push(ch - '0') ;
                 }
+                else if(ch == ')'){
 
-                oprtor.push(ch) ;
+                    while (oprtor.size() > 0 && oprtor.peek() != '(') {
+                        
+                        int v2 = oprnd.pop() ;
+                        int v1 = oprnd.pop() ;
+                        char opration = oprtor.pop() ;
+
+                        int result = operations(v1, v2, opration) ;
+                        oprnd.push(result) ;
+                    }
+
+                    oprtor.pop() ; // removing opening bracket
+
+                }
+                else if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+
+                    while (oprtor.size() > 0 && oprtor.peek() != '(' && precedance(oprtor.peek()) >= precedance(ch) ) {
+                        
+                        int v2 = oprnd.pop() ;
+                        int v1 = oprnd.pop() ;
+                        char opration = oprtor.pop() ;
+
+                        int result = operations(v1, v2, opration) ;
+                        oprnd.push(result) ;
+                    }
+
+                    oprtor.push(ch) ;
+                }
             }
+
+            while (oprtor.size() > 0) {
+                        
+                int v2 = oprnd.pop() ;
+                int v1 = oprnd.pop() ;
+                char opration = oprtor.pop() ;
+
+                int result = operations(v1, v2, opration) ;
+                oprnd.push(result) ;
+            }
+
+            System.out.println(oprnd.peek());
         }
-
-        while (oprtor.size() > 0) {
-                    
-            int v2 = oprnd.pop() ;
-            int v1 = oprnd.pop() ;
-            char opration = oprtor.pop() ;
-
-            int result = operations(v1, v2, opration) ;
-            oprnd.push(result) ;
-        }
-
-        System.out.println(oprnd.peek());
 
     }
 
