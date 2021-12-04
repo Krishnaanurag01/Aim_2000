d41c3109d039b5b1a996f5cad8053baf80f1245d
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-04 23:04:37 +0530

    DSA PROBLEM

---
+++ Stack_and_Queue_level01/Q12_Evaluation_of_Postfix_Expression.java
@@ -0,0 +1,57 @@
+package Stack_and_Queue_level01;
+import java.util.* ;
+
+public class Q12_Evaluation_of_Postfix_Expression {
+    
+    public static int evaluatePostFix(String str) {
+        
+        Stack<Integer> evaluate = new Stack<>() ;
+
+        for (int i = 0; i < str.length() ; i++) {
+            
+            char ch = str.charAt(i) ;
+
+            if(Character.isDigit(ch)){
+             
+                evaluate.push(ch - '0') ;
+            }
+            else{
+                // operations
+
+                // evaluating.
+                int v2 = evaluate.pop() ;
+                int v1 = evaluate.pop() ;
+                int result = getEvaluted(v1 , v2 , ch) ;
+
+                evaluate.push(result) ;
+
+            }
+        }
+
+
+
+
+        return evaluate.peek() ;
+
+    }
+
+    private static int getEvaluted(int v1, int v2, char opr) {
+
+        if(opr == '+'){
+            return v1 + v2 ;
+        }
+        else if(opr == '-'){
+            return v1 - v2 ;
+        }
+        else if(opr == '*'){
+            return v1 * v2 ;
+
+        }
+        else if(opr == '/'){
+            return v1 / v2 ;
+
+        }
+
+        return 0;
+    }
+}
