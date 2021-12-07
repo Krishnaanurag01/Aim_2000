09b410be3ba07929670d0b547648c1d7c520359c
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-07 23:49:22 +0530

    DSA PROBLEMS

---
+++ Stack_and_Queue_level01/Q25_Asteroid_Collision.java
@@ -0,0 +1,44 @@
+package Stack_and_Queue_level01;
+
+import java.util.* ;
+
+public class Q25_Asteroid_Collision {
+
+    public int[] asteroidCollision(int[] arr) {
+        
+        Stack<Integer> stack = new Stack<>() ;
+        
+        for(int val : arr){
+            
+            if( val > 0 ){
+                stack.push(val) ;
+            }
+            else{
+                
+                while(stack.size() > 0 && stack.peek() > 0 && stack.peek() < -val ){
+                    stack.pop();
+                }
+                if(stack.size() > 0 && stack.peek() > 0 && stack.peek() == -val ){
+                    stack.pop();
+                }
+                else if(stack.size() > 0 && stack.peek() > 0 && stack.peek() > -val ){
+                    
+                }
+                else{
+                    stack.push(val) ;
+                }
+                
+            }
+        }
+        
+        
+        int[] ans = new int[stack.size()] ;
+        
+        for(int i = stack.size()-1 ; i >= 0  ; i--){
+            ans[i] = stack.pop() ;
+        }
+        
+        return ans ;
+    }
+    
+}
