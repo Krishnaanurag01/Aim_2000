bb266928b9b7d2ce533023700a08d8d1df8b0124
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-08 22:11:35 +0530

    dsa problems

--- Problem_Of_The_day/Q20_Sum_of_two_elements_with_sum_nearest_to_zero.java
+++ Problem_Of_The_day/Q20_Sum_of_two_elements_with_sum_nearest_to_zero.java
@@ -1,5 +1,37 @@
 package Problem_Of_The_day;
 
 public class Q20_Sum_of_two_elements_with_sum_nearest_to_zero {
+
+    public static int closestToZero (int arr[], int n)
+    {
+        // your code here
+        Arrays.sort(arr) ;
+        
+        int ans = Integer.MAX_VALUE ;
+        
+        int i = 0 , j = n -1; 
+        
+        
+        while(i < j) {
+            int x  = arr[i] + arr[j] ;
+            
+            if(Math.abs(x) < Math.abs(ans) ){
+                ans = x ;
+            }
+            
+            if(Math.abs(x) == Math.abs(ans) ){
+                ans = Math.max(ans,x) ;
+            }
+            
+            if(x < 0 ){
+                i++ ;
+            }
+            else{
+                j-- ;
+            }
+        }
+        
+        return ans ;
+    }
     
 }
