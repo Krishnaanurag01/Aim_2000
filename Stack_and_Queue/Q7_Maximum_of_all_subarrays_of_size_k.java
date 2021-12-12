589984332748e8428a3f1ff9f59dcac3e0525c84
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-12 23:06:26 +0530

    DSA Problems

---
+++ Stack_and_Queue/Q7_Maximum_of_all_subarrays_of_size_k.java
@@ -0,0 +1,53 @@
+package Stack_and_Queue;
+import java.util.*;
+
+public class Q7_Maximum_of_all_subarrays_of_size_k {
+
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        ArrayList<Integer> ans = new ArrayList<>() ;
+
+        Stack<Integer> stack = new Stack<>() ;
+
+        stack.push(n-1) ;
+        int[] ng = new int[n] ;
+        ng[n-1] = n ;
+
+        for (int i = n-2 ; i >= 0 ; i--) {
+            
+            while (stack.size() > 0 && arr[stack.peek()] <= arr[i]) {
+                stack.pop() ;
+            }
+
+            if(stack.empty()){
+                ng[i] = n ;
+            }
+            else{
+                ng[i] = stack.peek() ;
+            }
+
+            stack.push(i) ;
+        }
+
+
+        int j = 0 ;
+
+        for (int i = 0; i <= ng.length - k ; i++) {
+            
+            if(j < i){
+                j = i ;
+            }
+
+            while (ng[j] < i + k) {
+                j = ng[j] ; // getting next greater value index .
+            }
+
+            ans.add(arr[j]) ; // adding value[j]
+        }
+
+
+
+        return ans ;
+    }
+    
+}
