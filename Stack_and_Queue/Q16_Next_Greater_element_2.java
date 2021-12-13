e8b491e8b6797529e50b89cc732c98e5bd4db45c
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-13 23:25:23 +0530

    DSA Problems

---
+++ Stack_and_Queue/Q16_Next_Greater_element_2.java
@@ -0,0 +1,38 @@
+package Stack_and_Queue;
+import java.util.*;
+
+public class Q16_Next_Greater_element_2 {
+    
+    public int[] nextGreaterElements(int[] nums) {
+        
+        int n = nums.length ;
+        
+        Stack<Integer> stack = new Stack<>() ; 
+        
+        // adding element from n-2 to 0
+        for(int i = n-2 ; i >= 0 ; i--){
+            
+            while(stack.size() > 0 && stack.peek() <= nums[i]){
+                stack.pop() ;
+            }
+            stack.push(nums[i]) ;
+        }
+        
+        int[] ans = new int[n] ;
+        
+        // now finding the answer from n-1 to 0.
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(stack.size() > 0 && stack.peek() <= nums[i]){
+                stack.pop() ;
+            }
+            
+            ans[i] = stack.size() > 0 ? stack.peek() : -1 ;
+            
+            stack.push(nums[i]);
+        }
+        
+        
+        return ans ;  
+
+    }
+}
