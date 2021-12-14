6c17c54bc6b0a6615e593dda6fde5863c2f683b4
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-14 20:57:46 +0530

    DSA PROBLEMS

---
+++ Stack_and_Queue/Q22_Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -0,0 +1,47 @@
+package Stack_and_Queue;
+import java.util.*;
+
+public class Q22_Minimum_Remove_to_Make_Valid_Parentheses {
+    
+    public String minRemoveToMakeValid(String s) {
+        
+        char[] str = s.toCharArray() ;
+        Stack<Integer> stack = new Stack<>() ;
+        
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            
+            char ch = s.charAt(i) ;
+            
+            if(ch == '('){
+                stack.push(i) ;
+            }
+            else  if(ch == ')'){
+                if(stack.size() == 0){
+                    str[i] = '.' ;
+                }
+                else{
+                    
+                    stack.pop() ;
+                }
+            }
+            
+        }
+        
+        
+        while( stack.size() > 0 ) {
+            str[stack.pop()] = '.' ;
+        }
+        
+        StringBuilder sb = new StringBuilder() ;
+        
+        for(char c : str){
+            if( c != '.'){
+                sb.append(c) ;
+            }
+        }
+        
+        return sb.toString() ;
+        
+    }
+}
