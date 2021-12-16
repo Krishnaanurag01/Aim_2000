66f78465df03c1c22b051c26bef30272546f7217
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-16 21:01:50 +0530

    DSA PROBLEMS

---
+++ Stack_and_Queue/Q37_Max_Stack.java
@@ -0,0 +1,63 @@
+package Stack_and_Queue;
+
+import java.util.Stack;
+
+public class Q37_Max_Stack {
+
+    public static class MaxStack {
+
+
+        Stack<Integer> data , max ; 
+
+        public MaxStack() {
+            data = new Stack<>() ;
+            max = new Stack<>() ;
+        }
+    
+        public void push(int x) {
+            data.push(x) ;
+            int m = x ;
+
+            if(max.size() > 0 ) m = Math.max(m, max.peek()) ;
+
+            max.push(m) ; // storing max value 
+    
+        }
+    
+        public int pop() {
+            max.pop() ;
+          return data.pop();
+        }
+    
+        public int top() {
+          return data.peek();
+        }
+    
+        public int peekMax() {
+          return max.peek();
+        }
+    
+        public int popMax() {
+
+            int mvsf = max.peek() ; // max value so far.
+
+            Stack<Integer> buffer = new Stack<>() ;
+
+            while (data.peek() != mvsf) {
+
+                buffer.push( pop()  ) ; // popping and pushing to buffer for backup
+            }
+
+            pop() ; // this is max element that we need to return
+
+
+            while (buffer.size() > 0) { // returning all backup values back to where it belongs.
+                push(buffer.pop());
+            }
+
+          return mvsf;
+        }
+    
+      }
+    
+}
