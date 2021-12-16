f04ee3a74943a94cfc07a5d9ad2cafb83526225c
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-16 21:02:41 +0530

    DSA PROBLEMS

---
+++ Stack_and_Queue/Q47_Implement_Queue_using_array.java
@@ -0,0 +1,32 @@
+package Stack_and_Queue;
+
+public class Q47_Implement_Queue_using_array {
+    class MyQueue {
+
+        int front, rear;
+        int arr[] = new int[100005];
+    
+        MyQueue()
+        {
+            front=0;
+            rear=0;
+        }
+        
+        //Function to push an element x in a queue.
+        void push(int x)
+        {
+            // Your code here
+            arr[rear++] = x ;
+            
+        } 
+    
+        //Function to pop an element from queue and return that element.
+        int pop()
+        {
+            // Your code here
+            if(front==rear) return - 1 ;
+            return arr[front++] ;
+        } 
+    }
+    
+}
