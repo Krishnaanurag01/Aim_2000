124f26338cb0e29e87bf1fb5cb142d533e95a1d1
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-12-22 21:47:39 +0530

    DSA PROBLEMS

--- Strings/Q18_LPS.java
+++ Strings/Q18_LPS.java
@@ -13,29 +13,31 @@ public class Q18_LPS {
         // check pep coding video for better explanation.
 
         int n = s.length();
-        int i = 1;
+
+        // i will denote index for lpa and start from 1 .
+        int i = 1; 
         int len = 0;
         int[] lps = new int[n];
 
-        // as string of length 1 can't have any prefix and suffix so its already zero thats why i is inialized with 1. and arr[0] = 0 by default.
+        // as string of length 1 can't have any prefix and suffix so its already zero thats why i is inialized with 1. and lps[0] = 0 by default.
 
         while (i < n) {
-            if(s.charAt(i) == s.charAt(len)){
-                len++;
-                lps[i] = len;
-                i++;
+            if(s.charAt(i) == s.charAt(len)){ // if they are same then
+                len++; // increase len 
+                lps[i] = len; // store in lps[i]
+                i++; // now increment i index
             }
             else{
-                if(len > 0){
-                    len = lps[len-1];
+                if(len > 0){ // if len is > 0 then find smaller len index
+                    len = lps[len-1]; // going back to smaller len
                 }
-                else{
+                else{ // if len becomes 0 then store 0 size and increment index.
                     lps[i] = 0 ;
                     i++;
                 }
             }
         }
-        return lps[n-1];
+        return lps[n-1]; // returning answer.
         
     }
     
