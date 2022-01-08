bd634c641e78cac65414219cbcbd8555474885aa
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-08 20:02:22 +0530

    DSA PROBLEMS

--- Greedy/Q21_Smallest_subset_with_sum_greater_than_all_other_elements.java
+++ Greedy/Q21_Smallest_subset_with_sum_greater_than_all_other_elements.java
@@ -7,21 +7,28 @@ public class Q21_Smallest_subset_with_sum_greater_than_all_other_elements {
 
     public static void main(String[] args) {
         
-        int[] arr = {1,2,2} ;
+        Integer[] arr = {1,2,3,4} ;
 
         minElements(arr, arr.length) ;
     }
 
 
-    static int minElements(int arr[], int n){
+    static int minElements(Integer[] arr, int n){
 
         Arrays.sort(arr);
 
+
+        System.out.println( Arrays.deepToString(arr) );
+
         int sum = 0 ;
+
 
         for (int i : arr) {
             sum += i ;
         }
+
+
+        System.out.println( "old sum -> " + sum );
 
         int count = 0 ;
         int msum = 0 ;
@@ -32,6 +39,10 @@ public class Q21_Smallest_subset_with_sum_greater_than_all_other_elements {
             msum += arr[i] ;
             count++ ;
             if (sum < msum) {
+        System.out.println( "new sum -> " + sum );
+        System.out.println( "my sum -> " + msum );
+
+
                 break ;
                 // return count ;
             }
