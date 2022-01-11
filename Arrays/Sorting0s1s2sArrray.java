622bdcc4d57de927dcd6bc33bb5582b2e9ee3f42
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-11 23:33:17 +0530

    DSA PROBLEMS

--- Arrays/Sorting0s1s2sArrray.java
+++ Arrays/Sorting0s1s2sArrray.java
@@ -37,5 +37,35 @@ public class Sorting0s1s2sArrray {
         }
         
     }
+
+
+    // method 02 :
+
+    public void sortColors(int[] nums) {
+        
+        int zero = 0 ;
+        int two  = nums.length - 1; 
+        
+        for(int i = 0 ; i < nums.length && i <= two ; i++ ){
+            
+            int val = nums[i] ;
+            
+            if(val == 0 ){ // when got 0 then swap with the zeroth index.
+                
+                nums[i] = nums[zero] ;
+                nums[zero] = val ;
+                zero++ ;
+                
+                
+            }
+            else if(val == 2){ // when got 2 then swap with twoth index and also do i-- so that current element will be checked again.
+                
+                nums[i] = nums[two] ;
+                nums[two] = val ;
+                two--;
+                i-- ;
+            }
+        }
+    }
     
 }
