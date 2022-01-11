c6482601768d07c59fa0bb4a3d169d43deab1bf8
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-11 23:32:33 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string/Q8_Product_of_Array_Except_Self.java
@@ -0,0 +1,34 @@
+package Arrays_and_string;
+
+public class Q8_Product_of_Array_Except_Self {
+    
+    public int[] productExceptSelf(int[] nums) {
+        
+        int n = nums.length ;
+        
+        int[] right = new int[n] ;
+        
+        right[n-1] = nums[n-1] ;
+        
+        for(int i = n - 2 ; i >= 0 ; i--){
+            right[i] = nums[i]*right[i+1] ; 
+        }
+        
+        int left_prefix = 1 ;
+        int[] ans = new int[n] ;
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            int left_is = left_prefix ;
+            int right_is = i < n - 1 ? right[i+1] : 1 ;
+            
+            ans[i] = left_is*right_is ;
+            
+            left_prefix *= nums[i] ;
+        }
+        
+    
+        return ans ;
+        
+    }
+}
