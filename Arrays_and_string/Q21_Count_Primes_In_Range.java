70f92539b9acb8fa6eb6452c03b6bc7ce1b7f0e9
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-13 23:05:02 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string/Q21_Count_Primes_In_Range.java
@@ -0,0 +1,41 @@
+package Arrays_and_string;
+
+import java.util.Arrays;
+
+
+// same logic as sieve of eratosthens.
+public class Q21_Count_Primes_In_Range {
+    
+    int countPrimes(int L, int N) {
+        
+        
+        boolean[] isPrime = new boolean[N+1] ;
+        Arrays.fill(isPrime,true) ;
+        
+        for(int i = 2 ; i * i  <= N ; i++ ){
+            if( isPrime[i] == true ) {
+                
+                for(int j = i + i ; j <= N ; j += i){
+                    isPrime[j] = false ;
+                }
+            }
+        }
+          
+          int count = 0 ;
+          
+          isPrime[0] = false ;
+          isPrime[1] = false ;
+        
+        for(int i = L ; i <= N ; i++){
+            
+            if(isPrime[i] == true){
+                count++;
+            }
+        }
+        
+        
+        return count ;
+        
+      }
+      
+}
