43b79a8fa559c4bd1c59310cc15ba656e7c822ff
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-17 23:37:53 +0530

    DSA PROBLEMS

--- Arrays_and_string/Q41_Minimum_Domino_Rotations_For_Equal_Row.java
+++ Arrays_and_string/Q41_Minimum_Domino_Rotations_For_Equal_Row.java
@@ -2,11 +2,15 @@ package Arrays_and_string;
 import java.util.*;
 
 public class Q41_Minimum_Domino_Rotations_For_Equal_Row {
+
+
+    // method 01 :
     
     public int minDominoRotations(int[] tops, int[] bottoms) {
         
-        int total = tops.length ;
+        int total = tops.length ; // this denotes that we must need to swap this much element in order to have a row completely filled with same number
         
+        // as dominos can have 1 to 6 numbers so making array of 7 length
         int[] arr1 = new int[7] ;
         int[] arr2 = new int[7] ;
         
@@ -14,10 +18,12 @@ public class Q41_Minimum_Domino_Rotations_For_Equal_Row {
             int val1 = tops[i] ;
             int val2 = bottoms[i] ;
             
-            if(val1 == val2){
+            if(val1 == val2){ // if both the elements are same then decrement the total
                 total-- ;
                 continue ;
             }
+
+            // else increase the freq.
             
             arr1[val1]++ ;
             arr2[val2]++ ;
@@ -25,35 +31,36 @@ public class Q41_Minimum_Domino_Rotations_For_Equal_Row {
         
         int ans = Integer.MAX_VALUE ;
         
-        ArrayList<Integer> list = new ArrayList<>() ;
+        ArrayList<Integer> list = new ArrayList<>() ; // this will contain the potential numbers
         
         for(int i = 0 ; i < arr1.length ; i++){
-            if(arr1[i] + arr2[i] == total){
+            if(arr1[i] + arr2[i] == total){ // if freq for i in both the array equal to total then add it to the list
                 list.add(i) ;
             }
         }
         
         // System.out.println(list) ;
         
-        int rest = tops.length - total ;
+        int rest = tops.length - total ; // this denotes that as we already have element which equals to total and this total got redued when same element appears in top and down array. so this will contains count of elements which comes in same index in both the arrays.
         
         // System.out.println(rest) ;
 
-        for(int i : list){
+        for(int i : list){ // check for potential ans
             int count = 0 ;
             for(int k = 0 ; k < tops.length ; k++){
-                if( i == tops[k] && tops[k] == bottoms[k]){
+                if( i == tops[k] && tops[k] == bottoms[k]){ // if the same element of top and bottom equals to potential ans then increase the count
                     count++ ;
                 }
             }
             
-            if(count == rest){
+            if(count == rest){ // if the count of potential ans is  equals to rest then update the ans and break
                 // System.out.println(count + " " + rest) ;
                 ans = Math.min(arr1[i],arr2[i]);
                 break ;
             }
         }
-        
+         
+    
         return ans == Integer.MAX_VALUE ? -1 : ans ;
         
     }
