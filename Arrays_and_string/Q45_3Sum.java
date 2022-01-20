ca03530e7ef235b034331217240b656b9323ffa2
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-20 22:59:01 +0530

    DSA PROBLEMS

--- Arrays_and_string/Q45_3Sum.java
+++ Arrays_and_string/Q45_3Sum.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Q45_3Sum {
 
-    public List<List<Integer>> threeSum(int[] nums) {
+   public List<List<Integer>> threeSum(int[] nums) {
         
         Arrays.sort(nums) ;
         
@@ -27,7 +27,7 @@ public class Q45_3Sum {
         
         int j = arr.length - 1 ;
         
-        while(i < j){
+        while (i < j){
             int total = arr[i] + firstVal + arr[j] ;
             
             if(total == 0){ // when the total becomes 0 then we do the following operations.
@@ -50,6 +50,72 @@ public class Q45_3Sum {
                     set.add(na) ;
                 }
                  i++ ;
+                j-- ;
+            }
+            else if(total < 0){
+                i++ ;
+            }
+            else{
+                j-- ;
+            }
+        }
+    }
+
+
+
+
+    // method 2 : optimal one!
+
+
+
+
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+        
+        Arrays.sort(nums) ;
+        
+        List<List<Integer>> ans = new ArrayList<>() ;
+        
+        for(int i = 0 ; i < nums.length-2 ; i++){
+            
+           if(i > 0 && nums[i-1] == nums[i]){ // if nums[i] == nums[i-1] then skip it.
+               continue;
+           }
+           twoSum(nums,i+1,nums[i],ans) ;
+            
+        }
+        
+        return ans ;
+        
+        
+    }
+  
+    public void twoSum2(int[] arr, int si , int firstVal, List<List<Integer>> ans){
+        
+        int j = arr.length - 1 ;
+        int i = si ;
+        
+        while(i < j){
+            
+            if( i != si && arr[i-1] == arr[i]) { /// ignore the first start index and after that check if arr[i] == arr[i-1] then skip.
+                i++ ;
+                continue ;
+            }
+        
+            
+            int total = arr[i] + firstVal + arr[j] ;
+            
+            if(total == 0){
+               
+                                  
+                List<Integer> in = new ArrayList<>() ;
+                in.add(firstVal) ;
+                in.add(arr[i]) ;
+                in.add(arr[j]) ;
+                
+                ans.add(in) ;
+                 
+                i++ ;
                 j-- ;
             }
             else if(total < 0){
