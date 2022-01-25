7960a7e350701ad58a1c3c1b72b1ec8977ef2ebc
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-01-25 23:48:53 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string/Q73_Number_of_Subarrays_With_Odd_Sum.java
@@ -0,0 +1,22 @@
+package Arrays_and_string;
+
+public class Q73_Number_of_Subarrays_With_Odd_Sum {
+
+    // method 01 : brute force
+
+    public int numOfSubarrays(int[] arr) {
+        
+        int count = 0 ;
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            int sum = 0 ;
+            for(int j = i ; j < arr.length ; j++){
+                sum += arr[j] ;
+                if(sum % 2 != 0) count++ ;
+            }
+        }
+        
+        return count ;
+    }
+
+}
