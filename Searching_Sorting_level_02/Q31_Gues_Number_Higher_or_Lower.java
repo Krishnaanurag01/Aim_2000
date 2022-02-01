0422b6366469cbf66d0e30d0ecbbc84bee332620
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-01 23:43:37 +0530

    DSA PROBLEMS

---
+++ Searching_Sorting_level_02/Q31_Gues_Number_Higher_or_Lower.java
@@ -0,0 +1,32 @@
+package Searching_Sorting_level_02;
+
+public class Q31_Gues_Number_Higher_or_Lower {
+
+    // same as Q30
+    
+    // public int guessNumber(int n) {
+        
+    //     int low = 0 ;
+    //     int high = n - 1 ;
+    //     int numberPicked = -1 ;
+        
+    //     while(low <= high){
+            
+    //         int tempPick = low + ( high - low )/2 ;
+            
+    //         //  int result = guess(tempPick) ; //  // this guess function is given by default in leetcode 374
+            
+    //         // if(result == 0){
+    //         //     return tempPick ;
+    //         // }
+    //         // else if(result == -1){
+    //         //     high = tempPick-1 ;
+    //         // }
+    //         // else{ // else 1
+    //         //     low = tempPick + 1 ;
+    //         // }
+    //     }
+        
+    //     return low ;
+    // }
+}
