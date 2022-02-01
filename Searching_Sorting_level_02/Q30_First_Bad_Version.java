f41d543d672f374803fc5af1fbbfa1447d16ba7a
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-01 23:43:29 +0530

    DSA PROBLEMS

---
+++ Searching_Sorting_level_02/Q30_First_Bad_Version.java
@@ -0,0 +1,30 @@
+package Searching_Sorting_level_02;
+
+public class Q30_First_Bad_Version {
+
+    // public int firstBadVersion(int n) {
+        
+    //     int low = 0 ;
+    //     int high = n ;
+        
+    //     int ans = -1 ;
+
+    //     // we are just finding the lowest version that is bad version
+        
+    //     while(low <= high ){
+            
+    //         int version = low  + ( high - low )/2 ;
+            
+    //         // if( isBadVersion(version) == true ){ // this function is given in leetcode 278
+    //         //     ans = version ; // updating answer
+    //         //     high = version - 1; // checking for lower version
+    //         // }
+    //         // else{
+    //         //     low = version + 1 ;
+    //         // }
+    //     }
+        
+    //     return ans ;
+    // }
+    
+}
