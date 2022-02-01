d71388e224de8ae092845fe4d561e534399a54f2
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-01 23:44:31 +0530

    DSA PROBLEMS

---
+++ Stack_and_Queue/Q61_Reverse_each_word_in_a_given_string.java
@@ -0,0 +1,40 @@
+package Stack_and_Queue; 
+
+import java.util.Stack;
+
+public class Q61_Reverse_each_word_in_a_given_string {
+
+    public static void main(String[] args) {
+
+        System.out.println( reverseWords("i.like.this.program.very.much") ) ;
+        
+    }
+
+    // we can solve this problem with stack.
+    static String reverseWords(String s)
+    {
+
+        Stack<Character> stack = new Stack<>() ; // creating a stack
+        StringBuilder sb = new StringBuilder() ; // this is used so that we can make our answer by adding char in o(1) time
+
+        for (int i = 0; i < s.length() ; i++) {
+            char ch = s.charAt(i) ;
+            if(ch != '.'){ // if it is not . then add it to the stack
+                stack.push(ch) ;
+            }
+            else{
+                while (stack.size() > 0) { // else if it is dot then pop every character and add it to the sb
+                    sb.append(stack.pop()) ;
+                }
+                sb.append(".") ; // also add it to the sb
+            }
+        }
+
+        while (stack.size() > 0) {
+            sb.append(stack.pop()) ; // now adding rest of the characters.
+        }
+
+        return sb.toString() ; // returning by making the string.
+    }
+
+}
