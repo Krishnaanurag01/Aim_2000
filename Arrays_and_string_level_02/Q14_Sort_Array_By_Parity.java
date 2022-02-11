dd4a62111488cabba1ecba1483e7bac193985fe5
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-11 22:28:06 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string_level_02/Q14_Sort_Array_By_Parity.java
@@ -0,0 +1,20 @@
+package Arrays_and_string_level_02;
+
+public class Q14_Sort_Array_By_Parity {
+    
+    public int[] sortArrayByParity(int[] nums) {
+        
+        int firstOdd = 0 ;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] % 2 == 0){ // swapping
+                int temp = nums[i] ;
+                nums[i] = nums[firstOdd] ;
+                nums[firstOdd] = temp ;
+                firstOdd++ ;
+            }
+        }
+     
+        return nums ;
+    }
+
+}
