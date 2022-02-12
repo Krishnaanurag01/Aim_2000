526f8e1f4b12fb52a696d328c259169868bd0f34
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-12 23:10:04 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string_level_02/Q24_Transpose_of_M_into_N_Matrix.java
@@ -0,0 +1,19 @@
+package Arrays_and_string_level_02;
+
+public class Q24_Transpose_of_M_into_N_Matrix {
+
+    public int[][] transpose(int[][] arr) {
+        
+        int n =  arr.length ;
+        int m = arr[0].length  ;
+        int[][] ans = new int[m][n] ; // for n * m size make an matrix of m *n .
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                ans[i][j] = arr[j][i] ;
+            }
+        }
+        return ans;
+    }
+    
+}
