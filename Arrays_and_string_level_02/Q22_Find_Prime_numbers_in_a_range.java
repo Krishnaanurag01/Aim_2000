cf34ded9b31884a103dd4cf6a9b1e07090f23396
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-12 23:09:49 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string_level_02/Q22_Find_Prime_numbers_in_a_range.java
@@ -0,0 +1,40 @@
+package Arrays_and_string_level_02;
+import java.util.*;
+
+// same logic as sieve of eratosthens.
+public class Q22_Find_Prime_numbers_in_a_range {
+    
+    ArrayList<Integer> primeRange(int L, int N) {
+        
+        
+        ArrayList<Integer> list = new ArrayList<Integer>() ;
+      
+       boolean[] isPrime = new boolean[N+1] ;
+     Arrays.fill(isPrime,true) ;
+     
+     for(int i = 2 ; i * i  <= N ; i++ ){
+         if( isPrime[i] == true ) {
+             
+             for(int j = i + i ; j <= N ; j += i){
+                 isPrime[j] = false ;
+             }
+         }
+     }
+      
+       
+       isPrime[0] = false ;
+       isPrime[1] = false ;
+     
+      for(int i = L ; i <= N ; i++){
+           
+           if(isPrime[i] == true){
+               list.add(i) ;
+           }
+       }
+     
+     
+     return list ;
+     
+   }
+
+}
