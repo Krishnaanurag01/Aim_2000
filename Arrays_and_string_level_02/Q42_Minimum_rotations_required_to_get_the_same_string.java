51bee7511a927547c4fd0e0eeb20a862ff37ac72
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-15 22:35:14 +0530

    DSA PROBLEMS

---
+++ Arrays_and_string_level_02/Q42_Minimum_rotations_required_to_get_the_same_string.java
@@ -0,0 +1,26 @@
+package Arrays_and_string_level_02;
+
+public class Q42_Minimum_rotations_required_to_get_the_same_string {
+
+    public static void main(String[] args) {
+        
+        System.out.println(getMinRotation("geeks"));
+    }
+
+    public static int getMinRotation(String s) {
+        
+        String ns = s + s ;
+
+        for (int i = 1; i < ns.length(); i++) { // starting from 1 becuase if we start from 0 then it give 0 as ns is made of itself so it'll return index of first occurance.
+            
+
+            if(ns.substring(i, i+s.length()).equals(s)){
+                return i ;
+            }
+
+        }
+
+        return 0 ;
+    }
+    
+}
