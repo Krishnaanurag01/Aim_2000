907a18db77736225a9594abdf1bb0e80f7de071c
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-24 23:45:13 +0530

    DSA PROBLEMS

---
+++ Searching_Sorting_level_02/Q40_Toppers_Of_Class.java
@@ -0,0 +1,21 @@
+package Searching_Sorting_level_02;
+// import java.util.*;
+
+public class Q40_Toppers_Of_Class {
+    
+    // void kTop(node arr[],int n)
+    // {
+        
+    //     Arrays.sort(arr, (a,b) -> {
+            
+    //         if(a.marks != b.marks){ // when marks are not same then sort in descending order
+    //             return b.marks - a.marks ;
+    //         }
+    //         else{ // when marks are same then sort based on ascending order of index.
+    //             return a.index - b.index ;
+    //         }
+            
+    //     }) ;
+    // }
+
+}
