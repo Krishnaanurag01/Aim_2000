361f894b83341be28b86ec705f9c26b8e6bb093c
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-02-27 22:25:39 +0530

    DSA PROBLEMS

---
+++ Graph_level_02/Q34_Euler_circuit_and_Path.java
@@ -0,0 +1,30 @@
+package Graph_level_02;
+import java.util.*;
+
+public class Q34_Euler_circuit_and_Path {
+
+    public int isEularCircuitExist(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+
+        int odd = 0 ; // counting odd number of degree for each node
+        
+        for(int i = 0 ; i < V ; i++){
+            if(adj.get(i).size() % 2 != 0 ) // if degree is odd then it means it is not circuit
+            {
+                odd++ ;
+            }
+            
+        }
+        
+        if(odd == 0 ){ // when no node has odd degree tthen it is circuit
+            return 2 ;
+        }
+        else if(odd == 2){ // when only two node has odd degree then it is path
+            return 1 ;
+        }
+         // else it is none.
+        return 0 ;
+
+    }
+    
+}
