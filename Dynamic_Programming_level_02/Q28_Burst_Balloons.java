cdf236bc84a22920dafeb214f529ec90b68198cf
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-03-06 06:53:55 +0530

    DSA PROBLEMS

--- Dynamic_Programming_level_02/Q28_Burst_Balloons.java
+++ Dynamic_Programming_level_02/Q28_Burst_Balloons.java
@@ -8,6 +8,8 @@ public class Q28_Burst_Balloons {
     }
 
     public static int get_max_profit(int[] arr) {
+
+        // here the logic is : agar current i balloon sabse last me futata to what will be the profit
         
 
         int[][] dp = new int[arr.length][arr.length] ;
@@ -17,9 +19,10 @@ public class Q28_Burst_Balloons {
             for (int i = 0, j = g ; j < arr.length; i++ , j++) {
                 
                 int max = Integer.MIN_VALUE ;
-                for (int k = i ; k <= j ; k++) {
-                    int left = k == i ? 0 : dp[i][k-1] ;
-                    int right = k == j ? 0 : dp[k+1][j] ; 
+                for (int k = i ; k <= j ; k++) { // going till j becuase we are bursting the jth balloon as well
+                    int left = k == i ? 0 : dp[i][k-1] ; // left side of k
+                    int right = k == j ? 0 : dp[k+1][j] ;  // right side of k
+                    // kth ballon 
                     int val = ( i == 0 ? 1 : arr[i-1] ) * arr[k] * ( j == arr.length-1 ? 1 : arr[j+1]) ;
 
                     int total = left + right + val ;
