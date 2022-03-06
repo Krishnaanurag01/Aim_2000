754e438e82708da949e087a0b24b1cdfebe3209f
Krishnaanurag01 <krishna.anurag02@gmail.com>
2022-03-06 06:54:32 +0530

    DSA PROBLEMS

--- Dynamic_Programming_level_02/Q43_Minimum_Cost_to_Make_Strings_Identical.java
+++ Dynamic_Programming_level_02/Q43_Minimum_Cost_to_Make_Strings_Identical.java
@@ -39,4 +39,43 @@ public class Q43_Minimum_Cost_to_Make_Strings_Identical {
 
         return ans ;
     }
+
+    // method 2 : Same as Q42 
+
+    public int findMinCost(String X, String Y, int costX, int costY)
+	{
+	    
+	    int[][] dp = new int[X.length() + 1 ][Y.length() + 1] ;
+	    
+	    for(int i = dp.length - 1 ; i>= 0 ; i--){
+	        for(int j = dp[0].length - 1; j >= 0 ; j--){
+	            
+	            if(i == dp.length - 1 && j == dp[0].length - 1 ){
+	                dp[i][j] = 0 ;
+	            }
+	            else if(i == dp.length - 1 ) {
+	                dp[i][j] = costY + dp[i][j+1] ;
+	            }
+	            else if( j == dp[0].length  - 1 ){
+	                dp[i][j] = costX + dp[i+1][j] ; 
+	            }
+	            else{
+	                
+	                char ch1 = X.charAt(i) ;
+	                char ch2 = Y.charAt(j) ;
+	                
+	                if(ch1 == ch2){
+	                    dp[i][j] = dp[i+1][j+1] ;
+	                }
+	                else{
+	                    dp[i][j] = Math.min( costX + dp[i+1][j] , costY + dp[i][j+1] );
+	                }
+	                
+	            }
+	            
+	        }
+	    }
+	    
+	    return dp[0][0];
+	}
 }
