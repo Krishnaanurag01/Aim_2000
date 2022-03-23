479fd77a3531e036d6b7dc9ce92c4bca52443162
Anurag Krishna <krishna.anurag02@gmail.com>
2022-03-23 23:51:24 +0530

    DSA PROBLEMS

--- Trie_Level_02/Q3_Word_Search_II.java
+++ Trie_Level_02/Q3_Word_Search_II.java
@@ -2,8 +2,98 @@ package Trie_Level_02;
 import java.util.*;
 
 public class Q3_Word_Search_II {
+
+    // optimal solution.
+
+     
+    static class Node{
+        
+        public Node[] childs = new Node[26] ;
+        public String s ; // this will store at the end point
+        int count = 0 ; // denotes total active child of current node 
+     }
+     
+     static List<String> list ;
+     
+     public List<String> findWords(char[][] board, String[] words) {
+         
+         Node root = new Node() ;
+         
+         // adding trie
+         for(String word : words){
+             insert(word,root) ;
+         }
+         
+         list = new ArrayList<>() ;
+         
+         // checking for each traversal
+         for(int i = 0; i < board.length ; i++){
+             for(int j = 0 ; j < board[0].length ; j++){
+                 
+                find(board,root,i,j) ;
+             }
+         }
+         
+         return list ;
+         
+     }
+     
+     void find(char[][] board, Node root, int i , int j){
+         
+        // out of bound, visited( '0') occurs then return without  doing anything
+         if( i < 0  || i >= board.length || j >= board[0].length || j < 0 || board[i][j] == '0' || root.childs[ board[i][j] - 'a' ] == null  )
+         {
+             return ;
+         }
+             
+         Node child = root.childs[ board[i][j] - 'a' ] ; // now getting the child node
+         
+         if(child.s != null){ // if it string is not null then it mmust be end point so add to answer.
+             list.add(child.s) ;
+             child.s = null ;
+         }
+         
+         char bkp = board[i][j] ; // backup
+         board[i][j] = '0' ; // visiting
+         
+         
+         // going adjecent directions
+         find(board,child,i+1,j) ;
+         find(board,child,i-1,j) ;
+         find(board,child,i,j+1) ;
+         find(board,child,i,j-1) ;
+         
+          board[i][j] = bkp ; // unvisiting 
+        
+         if(child.count == 0){
+             root.count-- ;
+         }
+         
+     }
+     
+     void insert(String word,Node root){
+         
+         Node curr = root ;
+         
+         for(int i = 0 ; i < word.length() ; i++){
+             char ch = word.charAt(i); 
+             
+             if(curr.childs[ch-'a'] == null){
+                 curr.childs[ch-'a'] = new Node() ;
+                 curr.count++ ;
+             }
+             
+             curr = curr.childs[ch-'a'] ;
+         }
+         
+         curr.s = word ;
+         // System.out.println(curr.s) ;
+     }
+ 
     
 }
+
+
 
 // SELF SOLUTION.
 // Algo : 
