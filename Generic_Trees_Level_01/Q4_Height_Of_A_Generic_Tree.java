ba25caf70bb419b3f5e6e61bbf3f034b97ebdd0d
Anurag Krishna <krishna.anurag02@gmail.com>
2022-03-24 23:24:57 +0530

    DSA PROBLEMS

---
+++ Generic_Trees_Level_01/Q4_Height_Of_A_Generic_Tree.java
@@ -0,0 +1,105 @@
+package Generic_Trees_Level_01;
+import java.util.*;
+import java.io.*;
+
+public class Q4_Height_Of_A_Generic_Tree {
+
+    
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node) {
+    int s = 0;
+
+    for (Node child : node.children) {
+      s += size(child);
+    }
+    s += 1;
+
+    return s;
+  }
+
+  public static int max(Node node) {
+    int m = Integer.MIN_VALUE;
+
+    for (Node child : node.children) {
+      int cm = max(child);
+      m = Math.max(m, cm);
+    }
+    m = Math.max(m, node.data);
+
+    return m;
+  }
+
+  
+  public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      int[] arr = new int[n];
+      String[] values = br.readLine().split(" ");
+      for (int i = 0; i < n; i++) {
+          arr[i] = Integer.parseInt(values[i]);
+        }
+        
+        Node root = construct(arr);
+        int h = height(root);
+        System.out.println(h);
+        // display(root);
+    }
+    
+    // height function : 
+
+    public static int height(Node node) {
+    
+      if(node.children.size() == 0){ // if no child then return 0
+        return 0 ;
+      }
+    
+      int height = 0 ;
+    
+      for (Node child : node.children ) { 
+        height = Math.max(height , height(child) ) ; // finding the max height of child.
+      }
+    
+      return 1 + height ; // adding 1 for current node.
+    }
+}
