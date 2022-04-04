218ee0bb978127ff22e580ef1248ae47aaeb94ea
Anurag Krishna <krishna.anurag02@gmail.com>
2022-04-04 23:25:23 +0530

    Resume Builder

--- Dynamic_Programming_extras/Q7_Triangle.java
+++ Dynamic_Programming_extras/Q7_Triangle.java
@@ -16,5 +16,82 @@ public class Q7_Triangle {
        int br = helper(idx+1,col+1,triangle); // going bottom with col+1 
         return Math.min(bottom,br) + triangle[idx][col]; // chosing min + cell value.
     }
+
+    // using memoization : 
+
+    public static int minimumPathSum2(int[][] triangle, int n) {
+        // Write your code here.
+        Integer[][] dp = new Integer[n][n] ; // using a dp
+        return helper( 0, 0 , triangle,dp);
+    }
     
+    static int helper(int idx,int col, int[][] triangle,Integer[][] dp){
+        if(idx == triangle.length - 1 ){
+          return dp[idx][col] = triangle[idx][col] ;  
+        } 
+        
+        if(dp[idx][col] != null) return dp[idx][col] ; // returning if already solved
+        
+       int bottom = helper(idx+1,col,triangle,dp);
+       int br = helper(idx+1,col+1,triangle,dp);
+        return dp[idx][col] = Math.min(bottom,br) + triangle[idx][col]; // storing answer in dp
+    }
+    
+
+
+    // tabulation :
+    
+    public static int minimumPathSum3(int[][] triangle, int n) {
+        // Write your code here.
+        int[][] dp = new int[n][n] ;
+        
+        for(int i = n - 1 ; i >= 0 ; i--){
+            for(int j = i ; j >=0 ; j--){
+                
+                if(i == n-1){ // the last row is base case so store the value as it is.
+                    dp[i][j] = triangle[i][j] ;
+                }
+                else{
+                     int bottom = triangle[i][j] + dp[i+1][j] ; // going down
+                     int bright =  triangle[i][j] + dp[i+1][j+1] ; // going bottom right
+                    
+                    dp[i][j] = Math.min(bottom,bright) ; // choosing min.
+                }
+            }
+        }
+        
+        
+        
+        return dp[0][0];
+    }
+
+
+    // space optimized solution : 
+
+    public static int minimumPathSum4(int[][] triangle, int n) {
+        // Write your code here.
+        int[] dp = new int[n] ;
+        
+        for(int i = n - 1 ; i >= 0 ; i--){
+            int[] temp = new int[n] ;
+            for(int j = i ; j >=0 ; j--){
+                
+                if(i == n-1){
+                    temp[j] = triangle[i][j] ;
+                }
+                else{
+                     int bottom = triangle[i][j] + dp[j] ;
+                     int bright =  triangle[i][j] + dp[j+1] ;
+                    
+                    temp[j] = Math.min(bottom,bright) ;
+                }
+            }
+            dp = temp ;
+        }
+        
+        
+        
+        return dp[0];
+    }
+
 }
