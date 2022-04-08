b4fc1cc4fbe8f417fa6c30ceb5d0db261104f810
Anurag Krishna <krishna.anurag02@gmail.com>
2022-04-08 23:57:51 +0530

    DSA PROBLEMS

---
+++ Bit_Manipulation/Q37_count_zero_xor_pairs.java
@@ -0,0 +1,26 @@
+package Bit_Manipulation;
+import java.util.*;
+
+public class Q37_count_zero_xor_pairs {
+
+    public static long calculate (int arr[], int n) {
+       
+        Arrays.sort(arr) ; // sorting becuase two same numbers xor is 0 so directly check adjecent numbers.
+        int res = 0 ;
+        int count = 0 ;
+        
+        for(int i = 1 ; i < n ; i++){
+            if( (arr[i] ^ arr[i-1]) == 0){
+                count++ ; 
+                res += count ;
+            }
+            else{
+                count = 0 ;
+            }
+        }
+        
+        return res ;
+     }
+     
+    
+}
