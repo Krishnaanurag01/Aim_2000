a06d49558f081b79389c0662d9abe315df981dfc
Anurag Krishna <krishna.anurag02@gmail.com>
2022-04-19 22:08:49 +0530

    DSA PROBLEMS

---
+++ Binary_Tree/food_distribution.java
@@ -0,0 +1,32 @@
+package Binary_Tree;
+
+import java.util.Scanner;
+
+public class food_distribution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in) ;
+
+        int p = sc.nextInt() ;
+        int q = sc.nextInt() ;
+
+        helper(p,q) ;
+    }
+
+    private static int helper(int p, int q) {
+
+        if( p > q){
+            return 0 ;
+        }
+        else if( p == q){
+            return q ;
+        }
+
+        int selfCount = q-1 ; 
+
+
+        return 0 ;
+    }
+    
+}
