e0588e4e67ad397078e37da43a6d8ea018f4f179
Anurag Krishna <krishna.anurag02@gmail.com>
2022-04-30 23:39:40 +0530

    DSA PROBLEMS

--- Binary_Tree/Q44_All_node_at_distance_k.java
+++ Binary_Tree/Q44_All_node_at_distance_k.java
@@ -124,3 +124,63 @@ public class Q44_All_node_at_distance_k {
     }
     
 }
+
+class TreeNode {
+    int val ;
+    TreeNode left ;
+    TreeNode right ;
+
+    TreeNode(int val){
+        this.val = val ;
+        this.left = null ;
+        this.right = null ;
+    }
+}
+
+// this is the optimnal solution : takes O(2n) time and O(1) space (excluding recursive space.)
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> ans = new ArrayList<>() ;
+        nodeToRoot(root,target,k,ans) ;
+        return ans ;        
+    }
+    
+    // using node to root path algo: and finding all the nodes k distance away from each and every parenr or path node of target.
+    int nodeToRoot(TreeNode root, TreeNode target, int k, List<Integer> ans){
+        if(root == null) return -1 ;
+        
+        if(root.val == target.val){
+            kdown(root,k,null,ans) ; // searching down of target node.
+            return 1 ;
+         }
+        
+        int ld = nodeToRoot(root.left, target,k,ans) ;
+        if(ld != -1){ // means left side has target node in its path so run kdown from here as well. it will find all the node that is k distance away from target and in between the current node and target node.
+            kdown(root,k-ld,root.left,ans) ; 
+            return 1+ld ;
+        }
+        
+        int rd = nodeToRoot(root.right, target,k,ans) ;
+        if(rd != -1){ // means right side has target node in its path so run kdown from here as well. it will find all the node that is k distance away from target and in between the current node and target node.
+            kdown(root,k-rd,root.right,ans) ;   
+            return 1+rd ;
+        }
+        
+        return -1 ;
+    }
+    
+    
+    // going k down.
+    void kdown(TreeNode root, int k , TreeNode blockedNode, List<Integer> ans){
+        
+        if(root == null || k < 0 || root == blockedNode) return ;
+        
+        if(k == 0){
+            ans.add(root.val) ;
+            return ;
+        }
+        
+        kdown(root.left ,k-1,blockedNode,ans) ;   
+        kdown(root.right ,k-1,blockedNode,ans) ;   
+    }
+}
