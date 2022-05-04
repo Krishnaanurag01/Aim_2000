86905ea1f712cf601448a1fb5ff7b245b999da10
Anurag Krishna <krishna.anurag02@gmail.com>
2022-05-04 23:45:03 +0530

    DSA PROBLEMS

---
+++ Dynamic_Programming_extras/Q75_Palindrome_Partitioning_II.java
@@ -0,0 +1,39 @@
+package Dynamic_Programming_extras ;
+
+public class Q75_Palindrome_Partitioning_II {
+
+
+    // using memoization.
+    static Integer[][] dp ;
+    public int minCut(String s) {
+        dp = new Integer[s.length()+1][s.length()+1] ;
+        return helper(0,s.length() ,s) - 1 ;
+    }
+    
+    int helper(int si, int ei, String s){
+        
+        if(si == ei) return  0 ; // both start and end is same then return 0 that denotes 0 partition.
+        if(dp[si][ei] != null) return dp[si][ei] ;
+        
+        int min = (int)1e9 ;
+        
+        for(int i = si ; i < ei; i++){
+            if(isPalind(si,i,s)){  // we will partiton the dp only when the partition string is palindrome.
+                int cost = 1 + helper(i+1,ei,s) ; // so finding the min cost.
+                min = Math.min(min,cost) ;
+            }
+        }
+        
+        return dp[si][ei] = min ;
+    }
+    
+    boolean isPalind(int si, int ei, String s){  // checks palindrome.
+        
+        while(si <= ei){
+            if(s.charAt(si) != s.charAt(ei)) return false;
+            si++ ;
+            ei-- ;
+        }
+        return true ;
+    }
+}
