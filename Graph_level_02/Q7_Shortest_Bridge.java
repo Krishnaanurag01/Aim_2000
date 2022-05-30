9650b2a4959603e487cd6e03740a355d72480480
Anurag Krishna <krishna.anurag02@gmail.com>
2022-05-30 18:46:40 +0530

    DSA PROBLEMS

--- Graph_level_02/Q7_Shortest_Bridge.java
+++ Graph_level_02/Q7_Shortest_Bridge.java
@@ -17,30 +17,29 @@ public class Q7_Shortest_Bridge {
         LinkedList<Pair> queue = new LinkedList<>();
         boolean[][] visited = new boolean[grid.length][grid[0].length] ;
 
-        boolean flag = false ;
+        boolean flag = false ; // helps in finding only one island.
         
         for(int i = 0 ; i < grid.length && flag == false ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
-                
                 if(grid[i][j] == 1){
-                  dfs(i,j,grid,queue,visited) ;  
+                  dfs(i,j,grid,queue,visited) ;   // dfs will add all the 1's of the current island.
                   flag = true ;
                   break ;
                 }
-                
             }
         }
         
         
+        // bfs starts from here.
+        
         int level = 0 ;
         
         while (queue.size() > 0) {
-
             int size = queue.size() ;
-            
             while (size-- > 0) {
                 Pair rem = queue.removeFirst();
                 
+                // adding the unvisited neighbours if they are not visited and not zero.
                 for (int i = 0; i < 4 ; i++) {
                     int rowDash = rem.x + directions[i][0];
                     int colDash = rem.y + directions[i][1];
@@ -49,11 +48,12 @@ public class Q7_Shortest_Bridge {
                         continue;
                     }
                     
-                    if(grid[rowDash][colDash] == 1){
+                    if(grid[rowDash][colDash] == 1){ // if one found then it is of second island because we have added all the ones of first island already.
                         return level ;
                     }
                     
-                    queue.addLast(new Pair(rowDash, colDash));
+                    visited[rowDash][colDash] = true ;
+                    queue.add(new Pair(rowDash, colDash));
                     
                 }
             }
@@ -71,10 +71,11 @@ public class Q7_Shortest_Bridge {
         {-1,0}
     };
 
+    // simple dfs adding all the adjecent 1's cell
     private void dfs(int i, int j, int[][] grid, LinkedList<Pair> queue, boolean[][] visited) {
 
         visited[i][j] = true ;
-        queue.addLast(new Pair(i, j));
+        queue.add(new Pair(i, j));
 
         for (int k = 0; k < 4 ; k++) {
             int rowDash = i + directions[k][0] ;
