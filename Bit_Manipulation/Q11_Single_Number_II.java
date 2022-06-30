ef4f6c1a5d547ec85374244cdd7075d477ee5c94
Anurag Krishna <krishna.anurag02@gmail.com>
2022-06-30 22:36:42 +0530

    DSA PROBLEMS

--- Bit_Manipulation/Q11_Single_Number_II.java
+++ Bit_Manipulation/Q11_Single_Number_II.java
@@ -3,6 +3,10 @@ package Bit_Manipulation;
 public class Q11_Single_Number_II {
     
     public int singleNumber(int[] nums) {
+
+        // tn -> 3n 
+        // tnp1 -> 3n+1
+        // tnp2 -> 3n+2
         
         int tn = -1 ; // tn
         int tnp1 = 0 ; //tn plus 1
@@ -10,12 +14,13 @@ public class Q11_Single_Number_II {
         
         for(int i = 0 ; i < nums.length ; i++){
             
-            int cwtn = tn & nums[i] ; // count of tn
+            // finding common
+            int cwtn = tn & nums[i] ; // count of tn 
             int cwtn1 = tnp1 & nums[i] ; // count og tn plus 1
             int cwtn2 = tnp2 & nums[i] ; // fro tn plus 2
             
-            tnp1 = tnp1 | cwtn ;
-            tn = tn & (~cwtn) ;
+            tnp1 = tnp1 | cwtn ; // turning on common bit in 3n+1 and
+            tn = tn & (~cwtn) ; // turning off common bit
             
             tnp2 = tnp2 | cwtn1 ;
             tnp1 = tnp1 & (~cwtn1) ;
