75e56e2870b1f44cb6d3da7c11d7c1e64e69936c
Anurag Krishna <krishna.anurag02@gmail.com>
2022-07-01 19:04:02 +0530

    DSA PROBLEMS

--- Bit_Manipulation/Q18_Sudoku_Using_Bit_Manipulation.java
+++ Bit_Manipulation/Q18_Sudoku_Using_Bit_Manipulation.java
@@ -1,75 +1,57 @@
 package Bit_Manipulation;
 
-import java.util.*;
-
 public class Q18_Sudoku_Using_Bit_Manipulation {
 
-  public static void display(int[][] arr){
-    for (int ii = 0; ii < arr.length; ii++) {
-      for (int jj = 0; jj < arr.length; jj++) {
-        System.out.print(arr[ii][jj] + " ");
-      }
-      System.out.println();
-    }
-    System.out.println();
-  }
+    boolean solved;
 
-  public static void main(String[] args) throws Exception {
-        Scanner scn = new Scanner(System.in);
-        int[][] arr = new int[9][9];
-        int[] rows = new int[9];
-        int[] cols = new int[9];
-        int[][] grid = new int[3][3];
+    public void solveSudoku(char[][] board) {
+        int[] row = new int[9]; // store visited val at thier bit position
+        int[] col = new int[9];
+        int[][] grid = new int[3][3]; // stores the identify of 3*3 matrix in one cell
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < 9; j++) {
-                int digit = scn.nextInt();
-                arr[i][j] = digit;
-                rows[i] = rows[i] | (1 << digit); // storing in row side for distinct
-                cols[j] = cols[j] | (1 << digit); // storing in column side for distinct
-                grid[i / 3][j / 3] = grid[i / 3][j / 3] | (1 << digit); // storing bits in 3*3 grid
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int val = board[i][j] - '0';
+                row[i] |= (1 << val);
+                col[j] |= (1 << val);
+                grid[i / 3][j / 3] |= (1 << val);
             }
-
         }
 
-        solveSudoku(arr, rows, cols, grid, 0, 0);
-        scn.close();
+        solved = false;
+        solution(board, row, col, grid, 0, 0);
     }
 
-    private static void solveSudoku(int[][] arr, int[] rows, int[] cols, int[][] grid, int i, int j) {
-
-        if (i >= arr.length) {
-            display(arr);
+    void solution(char[][] board, int[] row, int[] col, int[][] grid, int i, int j) {
+        if (i == board.length) { // base condition
+            solved = true; // turning the flag on
             return;
         }
 
-        // when it is already filled
-        if (arr[i][j] > 0) {   // moving to next row if on boundry
-            solveSudoku(arr, rows, cols, grid, j == arr.length - 1 ? i + 1 : i , j == arr.length - 1 ? 0 : j + 1 ); // then move forward without doing anything
-        }
-        else{ // at 0th place.
+        if (board[i][j] != '.') {
+            solution(board, row, col, grid, j == board[0].length - 1 ? i + 1 : i, j == board[0].length - 1 ? 0 : j + 1);
+        } else {
+            for (int num = 1; num <= 9; num++) {
+                if (((row[i] & (1 << num)) == 0) && ((col[j] & (1 << num)) == 0) && ((grid[i / 3][j / 3] & (1 << num)) == 0)) {
 
-            for (int num = 1 ; num <= 9 ; num++) {
-                
-                // checking in row , col and grid side if no one contains this number then proceeding further
-                if( ( rows[i] & ( 1 << num ) ) == 0   &&  (cols[j] & ( 1 << num ) ) == 0 && ( grid[i/3][j/3] & (1 << num) ) == 0  ){ // means not present.
+                    // visiting in row , col, grid
+                    board[i][j] = (char) (num + '0');
+                    row[i] ^= (1 << num);
+                    col[j] ^= (1 << num);
+                    grid[i / 3][j / 3] ^= (1 << num);
 
-                    arr[i][j] = num ;
-                    rows[i]= rows[i] ^ ( 1 << num ) ; // switching on in rows[i] side
-                    cols[j] = cols[j] ^ ( 1 << num) ; // switching on in cols[i] side
-                    grid[i/3][j/3]  ^= (1 << num) ;
-                    solveSudoku(arr, rows, cols, grid, j == arr.length - 1 ? i + 1 : i , j == arr.length - 1 ? 0 : j +1 ); // moving ahead
+                    solution(board, row, col, grid, j == board[0].length - 1 ? i + 1 : i,j == board[0].length - 1 ? 0 : j + 1);
 
-                    // unvisiting or switching off num'th bit.
-                    arr[i][j] = 0 ;
-                    rows[i]= rows[i] ^ ( 1 << num ) ; // switching off in rows[i] side
-                    cols[j] = cols[j] ^ ( 1 << num) ; // switching off in cols[i] side
-                    grid[i/3][j/3]  ^= (1 << num) ;
+                    if (solved) return; // if already solved then it will not perform any more operation
 
+                    // unvisiting
+                    board[i][j] = '.';
+                    row[i] ^= (1 << num);
+                    col[j] ^= (1 << num);
+                    grid[i / 3][j / 3] ^= (1 << num);
                 }
             }
         }
-        return ;
     }
 
 }
