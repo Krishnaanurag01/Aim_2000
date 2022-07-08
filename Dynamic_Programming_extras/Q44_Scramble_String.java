174170d991b227aa96e3172ab9c35e1253627c53
Anurag Krishna <krishna.anurag02@gmail.com>
2022-07-08 23:03:39 +0530

    DSA PROBLEMS

--- Dynamic_Programming_extras/Q44_Scramble_String.java
+++ Dynamic_Programming_extras/Q44_Scramble_String.java
@@ -51,7 +51,7 @@ public class Q44_Scramble_String {
 
             if(helper(s1.substring(0,i) , s2.substring(0,i), map)  &&
                helper(s1.substring(i) , s2.substring(i),map)  ){
-                map.put(key,true) ;  // storing and returning
+                map.put(key,true) ;  // storing and returning.
                 return true ;
             } 
             
