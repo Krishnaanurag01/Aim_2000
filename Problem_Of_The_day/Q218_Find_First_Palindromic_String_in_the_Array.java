92468b9633e07e755869f1bd2f9763fb651fa2ca
Anurag Krishna <krishna.anurag02@gmail.com>
2022-09-03 23:40:42 +0530

    Updated

---
+++ Problem_Of_The_day/Q218_Find_First_Palindromic_String_in_the_Array.java
@@ -0,0 +1,27 @@
+package Problem_Of_The_day;
+
+public class Q218_Find_First_Palindromic_String_in_the_Array {
+
+    public String firstPalindrome(String[] words) {
+     
+        for(String word : words){
+            if(isPalindrome(word)) return word;
+        }
+        
+        return "" ;
+    }
+    
+    boolean isPalindrome(String s){
+        
+        int i = 0 ; 
+        int j = s.length() - 1 ;
+        
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+}
