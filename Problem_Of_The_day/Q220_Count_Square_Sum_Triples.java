35a5d3c1bf3668b2e7f9c265e48c540856906b9c
Anurag Krishna <krishna.anurag02@gmail.com>
2022-09-05 23:39:45 +0530

    Updated

---
+++ Problem_Of_The_day/Q220_Count_Square_Sum_Triples.java
@@ -0,0 +1,16 @@
+package Problem_Of_The_day;
+
+public class Q220_Count_Square_Sum_Triples {
+
+    public int countTriples(int n) {
+
+        var count = 0;
+        for (var a = 1; a < n; a++)
+            for (var b = 1; b < n; b++) {
+                var c = Math.sqrt((a * a) + (b * b));
+                if (c == (int) c && c <= n)
+                    count++;
+            }
+        return count;
+    }
+}
