75b42ebf5bffef50a4508e158401c1c7795f6500
Anurag Krishna <krishna.anurag02@gmail.com>
2022-09-08 22:41:06 +0530

    Updated

---
+++ Problem_Of_The_day/Q222_Sum_of_k_smallest_elements_in_BST.java
@@ -0,0 +1,30 @@
+package Problem_Of_The_day;
+
+public class Q222_Sum_of_k_smallest_elements_in_BST {
+    int sum = 0;
+    int m ;
+    int sum(Node root, int k) { 
+        
+        m = k ;
+        helper(root) ;
+       return sum ;
+    } 
+    
+    void helper(Node root){
+        if(root == null) return ;
+        if(m == 0) return;
+        
+        helper(root.left) ;
+        if(m == 0) return ;
+        sum += root.data ;
+        m--;
+        helper(root.right) ;
+        
+    }    
+
+    class Node{
+        int data ;
+        Node left,right ;
+    }
+}
+
