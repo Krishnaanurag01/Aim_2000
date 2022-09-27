172ca10e8a5069be322a4ec351d0f2aeded7cda7
Anurag Krishna <krishna.anurag02@gmail.com>
2022-09-27 22:41:26 +0530

    Updated

---
+++ Problem_Of_The_day/Q239_Tom_and_Jerry.java
@@ -0,0 +1,9 @@
+package Problem_Of_The_day;
+
+public class Q239_Tom_and_Jerry {
+
+    static int numsGame(int N) {
+        // even hoga to tom jitega otherwise jerry jitega
+        return N % 2 == 0 ? 1 : 0 ;
+    }
+}
