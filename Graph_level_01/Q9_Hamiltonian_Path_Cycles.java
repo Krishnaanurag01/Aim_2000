e94550b49db71d98949d1b4f6ba29b92a175be5c
Anurag Krishna <krishna.anurag02@gmail.com>
2022-10-06 23:01:46 +0530

    DSA Problems

--- Graph_level_01/Q9_Hamiltonian_Path_Cycles.java
+++ Graph_level_01/Q9_Hamiltonian_Path_Cycles.java
@@ -1,5 +1,4 @@
 package Graph_level_01;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader; 
 import java.util.*;
@@ -21,9 +20,9 @@ public class Q9_Hamiltonian_Path_Cycles {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   
         int vtces = Integer.parseInt(br.readLine());
-        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
         for (int i = 0; i < vtces; i++) {
-           graph[i] = new ArrayList<>();
+           graph.add( new ArrayList<>());
         }
   
         int edges = Integer.parseInt(br.readLine());
@@ -32,8 +31,8 @@ public class Q9_Hamiltonian_Path_Cycles {
            int v1 = Integer.parseInt(parts[0]);
            int v2 = Integer.parseInt(parts[1]);
            int wt = Integer.parseInt(parts[2]);
-           graph[v1].add(new Edge(v1, v2, wt));
-           graph[v2].add(new Edge(v2, v1, wt));
+           graph.get(v1).add(new Edge(v1, v2, wt));
+           graph.get(v2).add(new Edge(v2, v1, wt));
         }
   
         int src = Integer.parseInt(br.readLine());
@@ -45,14 +44,13 @@ public class Q9_Hamiltonian_Path_Cycles {
         print_path(graph,src,visited , src +"",src) ;
      }
 
-    private static void print_path(ArrayList<Edge>[] graph, int src, HashSet<Integer> visited, String psf,int original_src) {
+    private static void print_path(ArrayList<ArrayList<Edge>> graph, int src, HashSet<Integer> visited, String psf,int original_src) {
 
-        if( visited.size() == graph.length - 1 ) { // the moment visited set becomes equal to graph.length -1 which means that visited set contains all the vertex of graph.
+        if( visited.size() == graph.size() - 1 ) { // the moment visited set becomes equal to graph.length -1 which means that visited set contains all the vertex of graph.
             System.out.print(psf); // so printint the path.
-
             boolean is_cycle = false ; // this will denotes if path is hamiltonian cycle.
 
-            for (Edge e : graph[src] ) { // if any of the edge node is equal to original source then it is cycle. 
+            for (Edge e : graph.get(src) ) { // if any of the edge node is equal to original source then it is cycle. 
                 if(e.nbr == original_src){
                     is_cycle = true ; // so make true and break the loop.
                     break ;
@@ -70,7 +68,7 @@ public class Q9_Hamiltonian_Path_Cycles {
 
         visited.add(src) ; // adding vertex to the set.
 
-        for (Edge e : graph[src]) { // running loop on all its edges.
+        for (Edge e : graph.get(src)) { // running loop on all its edges.
             if(visited.contains(e.nbr) == false){
                 print_path(graph, e.nbr , visited, psf + e.nbr, original_src);
             }
