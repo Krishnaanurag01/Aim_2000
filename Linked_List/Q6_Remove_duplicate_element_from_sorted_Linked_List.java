97ba1af65a7ffde84cfff17ff835f7a7c5941e65
Anurag Krishna <krishna.anurag02@gmail.com>
2022-10-10 23:42:20 +0530

    DSA Problems

--- Linked_List/Q6_Remove_duplicate_element_from_sorted_Linked_List.java
+++ Linked_List/Q6_Remove_duplicate_element_from_sorted_Linked_List.java
@@ -5,7 +5,6 @@ public class Q6_Remove_duplicate_element_from_sorted_Linked_List {
 
     node head ;
     public static class node {
-
         int data ;
         node next; 
 
@@ -15,97 +14,25 @@ public class Q6_Remove_duplicate_element_from_sorted_Linked_List {
         }        
     }
 
-
-    public void printLinkedList(node head) {
-
-        node printIt = head;
-        while(printIt != null){
-            System.out.print(printIt.data + " -> ");
-            printIt = printIt.next;
-        }
-
-        System.out.println();
-    }
-
-    public  void insertNode(int d) {
-        node newNode = new node(d);
-        if(head == null){
-            head = newNode ;
-            return;
-        }
-
-        node temp = head;
-
-        while (temp.next != null) {
-            temp = temp.next;            
-        }
-
-        temp.next = newNode;
-        
-    }
-
-
     // solution function.
-
-    public void removeDuplicate(node head2) {
-        if(head2 == null || head2.next == null){
-            return;
-        }
-
-        node current = head2;
-    
-        while (current.next!= null) {
-            if(current.data == current.next.data){
-                // node forward = current.next.next;
-                current.next= current.next.next;
-                // current = forward;
+    static node removeDuplicates(node head)
+    {
+        node nhead = new node(-1) ;
+        node ptr = nhead ;
+        node curr = head ;
+        
+        while(curr != null){
+            if(ptr.data == curr.data){
+                curr = curr.next ;
             }
             else{
-
-                current = current.next;
+                ptr.next = curr ;
+                ptr = ptr.next ;
+                curr = curr.next ;
             }
         }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void main(String[] args) {
-
-        Q6_Remove_duplicate_element_from_sorted_Linked_List ll = new Q6_Remove_duplicate_element_from_sorted_Linked_List();
-
-        ll.insertNode(2);
-        ll.insertNode(2);
-        ll.insertNode(2);
-        ll.insertNode(3);
-        ll.insertNode(3);
-        ll.insertNode(4);
-        ll.insertNode(25);
-        ll.insertNode(25);
-
-        ll.printLinkedList(ll.head);
-
-        ll.removeDuplicate(ll.head);
-
-        ll.printLinkedList(ll.head);
-
         
-    }
-    
+        ptr.next = null ;
+        return nhead.next ;
+    } 
 }
