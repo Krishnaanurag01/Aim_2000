0358c103fa71edc602d77a42626d302aae5db0f3
Anurag Krishna <krishna.anurag02@gmail.com>
2022-11-14 23:16:53 +0530

    DSA Problems

---
+++ LeetCode_Contests/Contest_319/Q1_Convert_the_Temperature.java
@@ -0,0 +1,7 @@
+package LeetCode_Contests.Contest_319;
+
+public class Q1_Convert_the_Temperature {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00} ;
+    }
+}
