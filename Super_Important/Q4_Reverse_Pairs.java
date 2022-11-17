bd655bb027c18f4358e45a01093ab4fb467a30a4
Anurag Krishna <krishna.anurag02@gmail.com>
2022-11-17 21:05:38 +0530

    DSA Problems

--- Super_Important/Q4_Reverse_Pairs.java
+++ Super_Important/Q4_Reverse_Pairs.java
@@ -5,29 +5,37 @@ public class Q4_Reverse_Pairs {
     
     public int reversePairs(int[] nums) {
         
+        // it uses merge sort.
+        
         return divide(nums, 0, nums.length - 1) ;
     }
     
+    // first divide part of merge sort
     int divide(int[] nums, int low, int high){
-        if(low >= high) return 0 ;
+        
+        if(low >= high) return 0 ; 
         int mid = (low + high)/2 ;
-        int count = divide(nums, low, mid);
-        count += divide(nums, mid+1, high);
-        count += merge(nums, low, mid, high);
+        int count = divide(nums, low, mid); // getting the reverse pair count in left part
+        count += divide(nums, mid+1, high); // now in right part
+        count += merge(nums, low, mid, high); // and now combine both and check pair count now.
         return count;
     }
     
     int merge(int[] nums, int low, int mid, int high){
+        
         int count = 0 ;
-        int i = low ;
-        for(int j = mid + 1 ; j <= high ; j++){    
-            while(i <= mid && (long)nums[j]*2 >= nums[i]){
+        
+        int i = low ; // left array start index.
+        for(int j = mid + 1 ; j <= high ; j++){  // traverse on right array.  
+            while(i <= mid && (long)nums[j]*2 >= nums[i]){ // if current element is lesser or equal then movee to next element of left
                 i++ ;
             }
             
+            // now current left element is one the answer and also the left is already sorted so all the element including current element is > 2*nums[j] so adding all element count in answer.
             count += (mid - i + 1) ;
         }
         
+        // now merge the both arrays.
         ArrayList<Integer> temp = new ArrayList<>() ;
         int left = low ;
         int right = mid + 1 ;
