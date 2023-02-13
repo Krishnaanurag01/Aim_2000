073ef45a5933d0a3b608d8957d690261ba899ea3
Anurag Krishna <krishna.anurag02@gmail.com>
2023-02-13 14:21:32 +0530

    DSA Problems

---
+++ Problem_Of_The_day/Q358_Arithmetic_Number.java
@@ -0,0 +1,10 @@
+package Problem_Of_The_day ;
+public class Q358_Arithmetic_Number {
+    
+    static int inSequence(int A, int B, int C){
+        if(A == B ) return 1;
+        if(C > 0 && (B - A) > 0 && (B - A) % C == 0 ) return 1 ;
+        if(C < 0 && (B - A) < 0 && (B - A) % C == 0 ) return 1 ;
+       return 0 ;
+    }
+}
